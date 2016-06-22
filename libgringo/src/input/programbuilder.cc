d9ca1fe1d1e3549c04ce5308848a34b4a950edc8
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2016-06-23 01:02:03 +0200

    just indentation

--- libgringo/src/input/programbuilder.cc
+++ libgringo/src/input/programbuilder.cc
@@ -1,22 +1,22 @@
-    // {{{ GPL License
+// {{{ GPL License
 
-    // This file is part of gringo - a grounder for logic programs.
-    // Copyright (C) 2013  Roland Kaminski
+// This file is part of gringo - a grounder for logic programs.
+// Copyright (C) 2013  Roland Kaminski
 
-    // This program is free software: you can redistribute it and/or modify
-    // it under the terms of the GNU General Public License as published by
-    // the Free Software Foundation, either version 3 of the License, or
-    // (at your option) any later version.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 
-    // This program is distributed in the hope that it will be useful,
-    // but WITHOUT ANY WARRANTY; without even the implied warranty of
-    // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    // GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 
-    // You should have received a copy of the GNU General Public License
-    // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    // }}}
+// }}}
 
 #include "gringo/input/programbuilder.hh"
 #include "gringo/input/literals.hh"
