6193b59774511e0c1d60cd848d353876cf38afea
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2016-09-28 17:22:01 +0200

    make run.py script compatible with run.sh

--- app/clingo/run.py
+++ app/clingo/run.py
@@ -12,25 +12,13 @@ clingo=os.path.normpath("{}/../../x64/ReleaseScript/clingo.exe".format(wd))
 def reorder(out):
     res = []
     current = []
-    for line in out.split('\n'):
-        if line.startswith("Step: "):
+    for line in out.splitlines():
+        if line.startswith("Step: ") or line.startswith("SAT") or line.startswith("UNSAT") or line.startswith("UNKNOWN") or line.startswith("OPTIMUM FOUND"):
             res.extend(sorted(current))
             res.append(line)
-        elif line.startswith("SAT"):
-            res.extend(sorted(current))
-            res.append(line)
-        elif line.startswith("UNSAT"):
-            res.extend(sorted(current))
-            res.append(line)
-        elif line.startswith("UNKNOWN"):
-            res.extend(sorted(current))
-            res.append(line)
-        elif line.startswith("OPTIMUM FOUND"):
-            res.extend(sorted(current))
-            res.append(line)
+            current = []
         else:
             current.append(" ".join(sorted(line.split(" "))))
-            pass
     return "\n".join(res)
 
 def normalize(out):
@@ -52,7 +40,10 @@ def normalize(out):
             state = 0
             current = []
         if state == 2:
-            current.append(line.strip().split(" "))
+            # TODO: the current script ignores empty models -> bad
+            model = line.strip()
+            if model != "":
+                current.append(model.split(" "))
             state=0
         elif line.startswith("Solving..."):
             state = 1
@@ -73,10 +64,15 @@ def normalize(out):
     norm.append(result)
     return "\n".join(norm)
 
+success = True
+
 for root, dirs, files in os.walk(wd):
     for f in files:
         if f.endswith(".lp"):
             b = os.path.join(root, f[:-3])
+            print "-" * 79
+            print b + ".lp"
+            print "." * 79
             args = [clingo, "0", b + ".lp", "-Wnone"]
             if os.path.exists(b + ".cmd"):
                 for x in open(b + ".cmd"):
@@ -85,13 +81,22 @@ for root, dirs, files in os.walk(wd):
             norm = normalize(out)
             sol  = reorder(open(b + ".sol").read())
             if norm != sol:
-                print "*" * (len(b) + 3)
-                print b + ".lp"
-                print "*" * (len(b) + 3)
+                print "failed"
+                print "!" * 79
                 d = dl.Differ()
-                for line in list(d.compare(norm.splitlines(1), sol.splitlines(1))):
+                for line in list(d.compare(sol.splitlines(), norm.splitlines())):
                     if not line.startswith(" "):
-                        sys.stdout.write(line)
-                sys.stdout.write("\n")
-                sys.stdout.flush()
+                        print line
+                success = False
+            else:
+                print "ok"
+
+print "-" * 79
+print "Result"
+print "." * 79
+if not success:
+    print "FAILED"
+    exit(1)
+else:
+    print "OK"
 
