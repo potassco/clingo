ac07d10cf87a3d05a4ec3f96a9653409b4f2cd26
rkaminsk <kaminski@cs.uni-potsdam.de>
2016-09-29 14:49:26 +0200

    GRINGO_ -> CLINGO_ in clingo.h

--- libgringo/clingo.h
+++ libgringo/clingo.h
@@ -50,19 +50,19 @@ extern "C" {
 #include <stdbool.h>
 
 #if defined _WIN32 || defined __CYGWIN__
-#   ifdef BUILDING_DLL
-#       define DLL_PUBLIC __declspec (dllexport)
+#   ifdef GRINGO_BUILD_LIBRARY
+#       define GRINGO_VISIBILITY_DEFAULT __declspec (dllexport)
 #   else
-#       define DLL_PUBLIC __declspec (dllimport)
+#       define GRINGO_VISIBILITY_DEFAULT __declspec (dllimport)
 #   endif
-#   define DLL_PRIVATE
+#   define GRINGO_VISIBILITY_PRIVATE
 #else
 #   if __GNUC__ >= 4
-#       define DLL_PUBLIC  __attribute__ ((visibility ("default")))
-#       define DLL_PRIVATE __attribute__ ((visibility ("hidden")))
+#       define GRINGO_VISIBILITY_DEFAULT  __attribute__ ((visibility ("default")))
+#       define GRINGO_VISIBILITY_PRIVATE __attribute__ ((visibility ("hidden")))
 #   else
-#       define DLL_PUBLIC
-#       define DLL_PRIVATE
+#       define GRINGO_VISIBILITY_DEFAULT
+#       define GRINGO_VISIBILITY_PRIVATE
 #   endif
 #endif
 
