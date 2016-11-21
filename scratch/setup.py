ae43752d250ed87a17eda39aa88f8308d110ec1f
rkaminsk <kaminski@cs.uni-potsdam.de>
2016-11-21 21:20:43 +0100

    fine tuning

--- scratch/setup.py
+++ scratch/setup.py
@@ -6,10 +6,11 @@ path = os.path.normpath(os.path.abspath(os.path.join(os.getcwd(), "build", "rele
 os.environ["LDFLAGS"] = "-Wl,-rpath={}".format(path)
 
 module = Extension('clingo',
+                   define_macros = [('PYCLINGO_NO_VISIBILITY', '1')],
                    sources = ['app/pyclingo/src/main.cc'],
                    include_dirs = ['libclingo', 'libpyclingo'],
                    libraries = ['pyclingo', 'clingo'],
-                   library_dirs = ['build/release/bin'],
+                   library_dirs = ['build/release/bin','build/release/lib'],
                    )
 
 setup (name = 'clingo',
