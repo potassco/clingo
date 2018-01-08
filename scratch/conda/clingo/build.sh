c6ede3c26301003a9fdb50846be2808d939004a6
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2018-01-08 17:05:59 +0100

    conda recipies for clingo
    - for now only supports osx-64 and linux-64

---
+++ scratch/conda/clingo/build.sh
@@ -0,0 +1,18 @@
+mkdir build
+cd build
+
+cmake .. \
+    -DCMAKE_CXX_COMPILER="${CXX}" \
+    -DCMAKE_C_COMPILER="${CC}" \
+    -DPYTHON_EXECUTABLE="${PYTHON}" \
+    -DCLINGO_REQUIRE_PYTHON=ON \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DPYCLINGO_USER_INSTALL=OFF \
+    -DCLINGO_BUILD_WITH_LUA=OFF \
+    -DCLINGO_MANAGE_RPATH=OFF \
+    -DCMAKE_BUILD_TYPE=Release
+
+make -j${CPU_COUNT}
+make install
+
+
