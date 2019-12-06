909fa1a602937f173a0459118cadb72612a00121
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2019-12-06 20:25:01 +0100

    fix doxygen comment

--- libclingo/clingo.h
+++ libclingo/clingo.h
@@ -1115,7 +1115,7 @@ CLINGO_VISIBILITY_DEFAULT bool clingo_propagate_init_add_literal(clingo_propagat
 CLINGO_VISIBILITY_DEFAULT bool clingo_propagate_init_add_clause(clingo_propagate_init_t *init, clingo_literal_t const *clause, size_t size, bool *result);
 //! Add the given weight constraint to the solver.
 //!
-//! This function add a constraint of form `literal == { lit=weight | (lit, weight) in literals } <= bound` to the solver.
+//! This function adds a constraint of form `literal == { lit=weight | (lit, weight) in literals } <= bound` to the solver.
 //!
 //! @attention No further calls on the init object or functions on the assignment should be called when the result of this method is false.
 //!
