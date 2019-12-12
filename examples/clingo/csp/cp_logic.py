45abb84da8fbbf3c064de32507119b5e98751b8c
ostrowsk <ostrowsk@cs.uni-potsdam.de>
2019-12-12 12:37:00 +0100

    completely not working

--- examples/clingo/csp/cp_logic.py
+++ examples/clingo/csp/cp_logic.py
@@ -1,4 +1,8 @@
-import clingo
+import clingo, math
+
+max_int = 100
+min_int = -100
+offset = 0-min_int
 
 class Constraint:
     def parse(self, term):
@@ -30,25 +34,53 @@ class Constraint:
 class State:
        def __init__(self):
            self.__dl = -1
-           self.__ub = []
-           self.__lb = []
+           self.__ub = {} # var -> [int]
+           self.__lb = {} # var -> [int]
+           self.__varval2orderlit = {}
+           self.__orderlit2varval = {}
        # {decisionLevel -> [lowerBounds,upperBounds]}
        def __str__(self):
            return "{}".format([self.__dl,self.__ub,self.__lb])
+       def init_domain(self, vars, true_lit):
+           self.__dl = 0
+           for v in vars:
+               self.__ub.setdefault(v, []).append(max_int)
+               self.__lb.setdefault(v, []).append(min_int)
+               self.__varval2orderlit[v] = [None]*(max_int-min_int+1)
+               self.set_literal(v,max_int,true_lit)
        def set_dl(self, dl):
-           for i in range(len(self.__ub),dl+1):
-               self.__ub.append({})
-               self.__lb.append({})
+           assert dl >= self.__dl
+           if dl > self.__dl:
+               for v in self.__ub.keys():
+                   cp_ub = self.__ub[v][self.__dl]
+                   cp_lb = self.__lb[v][self.__dl]
+                   for i in range(len(self.__ub[v]),dl+1):
+                       self.__ub[v].append(cp_ub)
+                       self.__lb[v].append(cp_lb)
            self.__dl = dl
-       def set_ub(self, var, value):
-           self.__ub[self.__dl][var] = value
-       def set_lb(self, var, value):
-           self.__lb[self.__dl][var] = value
+       def propagate_orderlits(self, control):
+           for v in self.__varval2orderlit:
+               print(self.__varval2orderlit[v])
+               print(self.__ub[v])
+               print(self.__lb[v])
+               lb_lit = (self.__varval2orderlit[v][self.lb(v)-1+offset])
+               for value in range(min_int+1,self.lb(v)):
+                   if self.__varval2orderlit[v][value-1+offset] != None:
+                       if not control.add_clause([lb_lit,-self.__varval2orderlit[v][value-1+offset]]):
+                           return False
+               assert self.__varval2orderlit[v][self.ub(v)+offset] != None
+               ub_lit = self.__varval2orderlit[v][self.ub(v)+offset]
+               for value in range(self.ub(v)+1, max_int):
+                   if self.__varvar2orderlit(v)[value+offset] != None:
+                       if not control.add_clause([-ub_lit,self.__varvar2orderlit(v)[value+offset]]):
+                           return False
+           return True
+
        def backtrack(self, dl):
            print("backtrack dl", dl)
            self.__ub = self.__ub[:dl]
            self.__lb = self.__lb[:dl]
-           self.__dl = -1
+           self.__dl = dl-1
        def propagate_true(self, c):
            return []
        def propagate_false(self, c):
@@ -56,15 +88,23 @@ class State:
        def propagate_free(self, c):
            return []
        def lb(self, var):
-           if var in self.__lb[self.__dl]:
-               return self.__lb[self.__dl][var]
-           else:
-               return None
+           return self.__lb[var][self.__dl]
        def ub(self, var):
-           if var in self.__ub[self.__dl]:
-               return self.__ub[self.__dl][var]
+           return self.__ub[var][self.__dl]
+       def set_literal(self, var, value, lit):
+           self.__varval2orderlit[var][value+offset] = lit
+           self.__orderlit2varval.setdefault(lit, []).append((var,value))
+       def update_domain(self, order_lit): #literal is always true, may need negation to be found
+           if order_lit in self.__orderlit2varval:
+               for (var,value) in self.__orderlit2varval[order_lit]:
+                   if self.ub(var) >= value:
+                       self.__ub[var][self.__dl] = value
            else:
-               return None
+               assert (-order_lit in self.__orderlit2varval)
+               for (var,value) in self.__orderlit2varval[-order_lit]:
+                   if self.lb(var) <= value:
+                       self.__lb[var][self.__dl] = value
+           
           
 
 class Propagator:
@@ -89,13 +129,26 @@ class Propagator:
                 self.__c2l.setdefault(c, []).append(lit)
                 for (a,v) in c.vars():
                     self.__vars.add(v)
+        true_lit = init.add_literal()
+        init.add_clause([true_lit])
+        self.__state(0).init_domain(self.__vars, true_lit)
+        for i in range(1,init.number_of_threads):
+           self.__state(i)
+           self.__states[thread_id] = self.__state(0)
 
 #                init.add_watch(lit)
+    def propagate(self, control, changes):
+        s = self.__state(control.thread_id)
+        for i in changes:
+            s.update_domain(i)
+
 
     def check(self, control):
         change = True
         state = self.__state(control.thread_id)
         state.set_dl(control.assignment.decision_level)
+        if not state.propagate_orderlits(control):
+            return
         while (change):
             change = False
             for l,c in self.__l2c.items(): # bad?
@@ -107,14 +160,29 @@ class Propagator:
                     cl = state.propagate_free(c)
                 for clause in cl:
                     change = True
-                    control.add_clause(clause)
+                    if not control.add_clause(clause):
+                        return
             if not control.propagate():
                 return
+            if not state.propagate_orderlits(control):
+                return
+        if control.assignment.is_total:
+            return self.check_full(control)
         return
+    def check_full(self, control):
+        s = self.__state(control.thread_id)
+        for v in self.__vars:
+            if s.lb(v) != s.ub(v):
+                l = control.add_literal()
+                s.set_literal(v,s.lb(v)+ math.floor((s.ub(v)-s.lb(v))/2), l)
+                control.add_watch(l)
+                control.add_watch(-l)
+                break
+        
 
     def undo(self, thread_id, assign, changes):
         self.__state(thread_id).backtrack(assign.decision_level)
 
     def get_assignment(self, thread_id):
         s = self.__state(thread_id)
-        return [(str(x),s.lb(x)) for x in self.__vars if s.lb(x) == s.ub(x) and s.lb(x)!=None] 
+        return [(str(x),s.lb(x)) for x in self.__vars if s.lb(x) == s.ub(x)] 
