e629473ef71fe47feb4ef929103f30260af352c2
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2020-01-11 22:29:09 +0100

    another try to make the travesty happy

--- libpyclingo/pyclingo.cc
+++ libpyclingo/pyclingo.cc
@@ -54,8 +54,46 @@
 #else
 #define OBBASE(x) x
 #define Py_hash_t long
-#define PySlice_Unpack _PySlice_Unpack
-#define PySlice_AdjustIndices _PySlice_AdjustIndices
+
+int PySlice_Unpack(PyObject *slice, Py_ssize_t *start, Py_ssize_t *stop, Py_ssize_t *step) {
+    Py_ssize_t length;
+    return PySlice_GetIndicesEx(reinterpret_cast<PySliceObject*>(slice), PY_SSIZE_T_MAX, start, stop, step, &length);
+}
+
+Py_ssize_t PySlice_AdjustIndices(Py_ssize_t length, Py_ssize_t *start, Py_ssize_t *stop, Py_ssize_t step) {
+    assert(step != 0);
+    assert(step >= -PY_SSIZE_T_MAX);
+    if (*start < 0) {
+        *start += length;
+        if (*start < 0) {
+            *start = (step < 0) ? -1 : 0;
+        }
+    }
+    else if (*start >= length) {
+        *start = (step < 0) ? length - 1 : length;
+    }
+    if (*stop < 0) {
+        *stop += length;
+        if (*stop < 0) {
+            *stop = (step < 0) ? -1 : 0;
+        }
+    }
+    else if (*stop >= length) {
+        *stop = (step < 0) ? length - 1 : length;
+    }
+    if (step < 0) {
+        if (*stop < *start) {
+            return (*start - *stop - 1) / (-step) + 1;
+        }
+    }
+    else {
+        if (*start < *stop) {
+            return (*stop - *start - 1) / step + 1;
+        }
+    }
+    return 0;
+}
+
 #endif
 
 #ifndef PyVarObject_HEAD_INIT
