3dbd76bfebaadea63a0f6b7e4d8e05cd79b6087d
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2020-01-12 22:02:17 +0100

    fix slicing with old python versions

--- libpyclingo/pyclingo.cc
+++ libpyclingo/pyclingo.cc
@@ -57,50 +57,14 @@
 #endif
 
 #if PY_VERSION_HEX < 0x03060100
-
-int SliceUnpack(PyObject *slice, Py_ssize_t *start, Py_ssize_t *stop, Py_ssize_t *step) {
-    Py_ssize_t length;
-    return PySlice_GetIndicesEx(reinterpret_cast<PySliceObject*>(slice), PY_SSIZE_T_MAX, start, stop, step, &length);
-}
-
-Py_ssize_t SliceAdjustIndices(Py_ssize_t length, Py_ssize_t *start, Py_ssize_t *stop, Py_ssize_t step) {
-    assert(step != 0);
-    assert(step >= -PY_SSIZE_T_MAX);
-    if (*start < 0) {
-        *start += length;
-        if (*start < 0) {
-            *start = (step < 0) ? -1 : 0;
-        }
-    }
-    else if (*start >= length) {
-        *start = (step < 0) ? length - 1 : length;
-    }
-    if (*stop < 0) {
-        *stop += length;
-        if (*stop < 0) {
-            *stop = (step < 0) ? -1 : 0;
-        }
-    }
-    else if (*stop >= length) {
-        *stop = (step < 0) ? length - 1 : length;
-    }
-    if (step < 0) {
-        if (*stop < *start) {
-            return (*start - *stop - 1) / (-step) + 1;
-        }
-    }
-    else {
-        if (*start < *stop) {
-            return (*stop - *start - 1) / step + 1;
-        }
-    }
-    return 0;
-}
+// Note: it is impossible to get around using these functions without keeping
+// the slice object around. Thus, clingo only supports python versions
+// providing them.
+#define SliceUnpack _PySlice_Unpack
+#define SliceAdjustIndices _PySlice_AdjustIndices
 #else
-
 #define SliceUnpack PySlice_Unpack
 #define SliceAdjustIndices PySlice_AdjustIndices
-
 #endif
 
 #ifndef PyVarObject_HEAD_INIT
