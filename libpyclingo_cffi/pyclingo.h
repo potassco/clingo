536d67a910856a9b71ff784aed2e61a5001cf73d
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2020-12-14 15:04:08 +0100

    add support for building python library using cffi

---
+++ libpyclingo_cffi/pyclingo.h
@@ -0,0 +1,64 @@
+// {{{ MIT License
+
+// Copyright 2017 Roland Kaminski
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+// }}}
+
+#ifndef PYCLINGO_CLINGO_H
+#define PYCLINGO_CLINGO_H
+
+#include <clingo/script.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32)
+#  define CFFI_DLLEXPORT  __declspec(dllexport)
+#elif defined(__GNUC__)
+#  define CFFI_DLLEXPORT  __attribute__((visibility("default")))
+#else
+#  define CFFI_DLLEXPORT  /* nothing */
+#endif
+
+CFFI_DLLEXPORT bool pyclingo_execute_(clingo_location_t const *loc, char const *code, void *);
+CFFI_DLLEXPORT bool pyclingo_call_(clingo_location_t const *loc, char const *name, clingo_symbol_t const *arguments, size_t size, clingo_symbol_callback_t symbol_callback, void *symbol_callback_data, void *);
+CFFI_DLLEXPORT bool pyclingo_callable_(char const * name, bool *ret, void *);
+CFFI_DLLEXPORT bool pyclingo_main_(clingo_control_t *ctl, void *);
+inline void pyclingo_free_(void *) { }
+
+inline bool clingo_register_python_() {
+    clingo_script_t_ script = {
+        pyclingo_execute_,
+        pyclingo_call_,
+        pyclingo_callable_,
+        pyclingo_main_,
+        pyclingo_free_,
+        "TODO: This has to be set via a definition."
+    };
+    return clingo_register_script_(clingo_ast_script_type_python, &script, NULL);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // PYCLINGO_CLINGO_H
