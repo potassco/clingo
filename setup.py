db8ffefe11e2883b60f37945a8566fc2d81ba7f8
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2020-12-30 21:47:26 +0100

    add a project description

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
 import sys
 import site
+from textwrap import dedent
 from skbuild import setup
 
 if not site.ENABLE_USER_SITE and "--user" in sys.argv[1:]:
     site.ENABLE_USER_SITE = True
 
 setup(
-    version = '5.5.0.post13',
+    version = '5.5.0.post15',
     name = 'clingo-cffi',
-    description = 'A grounder and solver for logic programs.',
+    description = 'CFFI-based bindings to the clingo solver.',
+    long_description = dedent('''\
+        This package provides CFFI-based bindings to the clingo solver.
+
+        Clingo is part of the [Potassco](https://potassco.org) project for *Answer Set Programming* (ASP).
+        ASP offers a simple and powerful modeling language to describe combinatorial problems as *logic programs*.
+        The *clingo* system then takes such a logic program and computes *answer sets* representing solutions to the given problem.
+        To get an idea, check our [Getting Started](https://potassco.org/doc/start/) page and the [online version](https://potassco.org/clingo/run/) of clingo.
+
+        Temporarily, the API documentation of this project can be found [here](https://www.cs.uni-potsdam.de/~kaminski/pyclingo-cffi/).
+        '''),
+    long_description_content_type='text/markdown',
     author = 'Roland Kaminski',
+    author_email = 'kaminski@cs.uni-potsdam.de',
     license = 'MIT',
     url = 'https://github.com/potassco/clingo',
     install_requires=[ 'cffi' ],
