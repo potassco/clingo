1e87df0e2fa16243d5e1ac60798adbc9cf137e16
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2021-01-14 19:21:43 +0100

    fix twine

--- .github/pipwindows.py
+++ .github/pipwindows.py
@@ -5,19 +5,7 @@ Script to build binary wheels on windows.
 from re import finditer, escape, match, sub
 from subprocess import check_call
 from urllib.request import urlopen
-from os import environ
 from glob import glob
-
-def setup():
-    with open('twine.cfg', 'w') as fh:
-        fh.write('''\
-[distutils]
-index-servers=clingo-cffi
-[clingo-cffi]
-repository = https://test.pypi.org/legacy/
-username=__token__
-password={}
-'''.format(environ['PYPI_API_TOKEN']))
 
 def adjust_version():
     '''
@@ -52,8 +40,7 @@ def adjust_version():
 
 
 if __name__ == "__main__":
-    setup()
     adjust_version()
     check_call(['python', 'setup.py', 'bdist_wheel'])
     for wheel in glob('dist/*.whl'):
-        check_call(['python', '-m', 'twine', '--config-file', "twine.cfg", '--repository', 'clingo-cffi', wheel])
+        check_call(['python', '-m', 'twine', 'upload', wheel])
