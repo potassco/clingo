bb80d2a830139349f66c42ee09caca99d895fbf0
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2021-03-05 20:03:47 +0100

    fix compilation

--- .github/conda.py
+++ .github/conda.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python
+'''
+Simple script to call conda build with the current revision and version.
+'''
 
 import subprocess
 import json
 import re
 import os
-import locale
 
 LABEL = "dev"
 CHANNELS = ["potassco", "potassco/label/dev"]
 NAME = "clingo"
 
 def get_version():
+    '''
+    Get the current version.
+    '''
     with open('libclingo/clingo.h') as f:
         text = f.read()
     m = next(re.finditer(r'#define CLINGO_VERSION "([0-9]*\.[0-9]*\.[0-9*])"', text))
     return m.group(1)
 
 def get_build_number(version):
+    '''
+    Get the next build number.
+    '''
     pkgs = json.loads(subprocess.check_output(['conda', 'search', '--json', '-c', CHANNELS[-1], NAME]))
 
     build_number = -1
@@ -27,6 +35,9 @@ def get_build_number(version):
     return build_number + 1
 
 def run():
+    '''
+    Compile and upload conda packages.
+    '''
     version = get_version()
     build_number = get_build_number(version)
 
@@ -37,13 +48,13 @@ def run():
     if 'GITHUB_SHA' in os.environ:
         build_env["BUILD_REVISION"] = os.environ['GITHUB_SHA']
 
+    recipe_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'conda')
     options = ['conda', 'build', '--label', LABEL]
     for c in CHANNELS:
         options.extend(['-c', c])
-    options.append('.')
+    options.append(recipe_path)
 
-    d = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'conda')
-    subprocess.call(options + [d], env=build_env)
+    subprocess.call(options, env=build_env)
 
 if __name__ == '__main__':
     run()
