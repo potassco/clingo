d774837be6fa951c1930eaeb024ce96407338a0c
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2021-04-20 16:18:34 +0200

    fine-tune deploy script

--- .github/trigger.sh
+++ .github/trigger.sh
@@ -17,27 +17,29 @@ function dispatch() {
       -X POST \
       -H "Accept: application/vnd.github.v3+json" \
       "https://api.github.com/repos/potassco/clingo/actions/workflows/$1/dispatches" \
-      -d "{\"ref\":\"${dev_branch}\"}"
+      -d "{\"ref\":\"${dev_branch}\",\"wip\":$2}"
 }
+
+wip=true
 
 case $1 in
     list)
         list
         ;;
+    release)
+        wip=false
+        ;&
     dev)
         # .github/workflows/manylinux.yml
-        dispatch 4811844
+        dispatch 4811844 $wip
         # .github/workflows/pipsource.yml
-        dispatch 4823035
+        dispatch 4823035 $wip
         # .github/workflows/ppa-dev.yml
-        dispatch 4881510
+        dispatch 4881510 $wip
         # .github/workflows/conda-dev.yml
-        dispatch 4923491
+        dispatch 4923491 $wip
         # .github/workflows/pipwinmac-wip.yml
-        dispatch 4978730
-        ;;
-    release)
-        echo "implement me"
+        dispatch 4978730 $wip
         ;;
     *)
         echo "usage: trigger {list,dev,release}"
