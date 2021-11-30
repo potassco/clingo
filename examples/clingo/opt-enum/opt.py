3e13690a7039aec1e19600b793e03ac360b8909c
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2021-11-30 17:44:39 +0100

    refine a bit

--- examples/clingo/opt-enum/opt.py
+++ examples/clingo/opt-enum/opt.py
@@ -1,8 +1,18 @@
+"""
+Simple example showing how to resume optimization to enumerate all optimal
+values in lexicographical order.
+
+# TODO
+- add an option to control whether to restore the last solution candidate
+- add constraints to establish upper bounds with more than one priority
+- maybe print the models in a nicer way
+- maybe add the number of optimal models to the user statistics
+"""
 import sys
-from typing import cast, Sequence, Tuple
+from typing import cast, Dict, List, Optional, Sequence, Tuple
 
 from clingo.configuration import Configuration
-from clingo.backend import Observer
+from clingo.backend import Backend, Observer
 from clingo.control import Control
 from clingo.application import Application
 from clingo.solving import SolveResult, Model
@@ -11,34 +21,34 @@ from clingo import clingo_main
 
 
 class MinObs(Observer):
+    literals: Dict[int, List[Tuple[int, int]]]
+
     def __init__(self):
-        self.literals = None
+        self.literals = {}
 
     def minimize(self, priority: int, literals: Sequence[Tuple[int,int]]):
-        if priority != 0:
-            raise RuntimeError('only priority level 0 accepted for now')
-
-        if self.literals is None:
-            self.literals = []
-
-        self.literals.extend(literals)
-
+        self.literals.setdefault(priority, []).extend(literals)
 
 class RestoreHeu(Propagator):
+    costs: Dict[Tuple[int], Sequence[int]]
+    models: Dict[int, Sequence[int]]
+    restore: Optional[Sequence[int]]
+
     def __init__(self):
         self.costs = {}
         self.models = {}
         self.restore = None
 
-    def set_restore(self, bound: int):
-        min_cost = None
+    def set_restore(self, bound: Tuple[int]):
+        min_costs = bound
         for costs, model in self.costs.items():
-            if costs[0] > bound and (min_cost is None or min_cost > costs[0]):
-                min_cost = costs[0]
+            if bound < costs < min_costs:
+                min_costs = costs
                 self.restore = model
 
     def on_model(self, model: Model):
-        self.costs[tuple(model.cost)] = self.models[model.thread_id]
+        idx = cast(Tuple[int], tuple(model.cost))
+        self.costs[idx] = self.models[model.thread_id]
 
     def check(self, control: PropagateControl):
         self.models[control.thread_id] = list(control.assignment.trail)
@@ -53,25 +63,51 @@ class RestoreHeu(Propagator):
 
 
 class OptApt(Application):
+    _restore: bool
+
     def __init__(self):
-        self._bound = None
+        # TODO: this should be controlled via an option
+        self._restore = True
+
+    def _set_upper_bound(self, backend: Backend, minimize: Sequence[Sequence[Tuple[int, int]]], bound: Sequence[int]):
+        assert len(minimize) == len(bound)
+        if len(minimize) != 1:
+            raise RuntimeError('only one priority level accepted for now')
+
+        # TODO: multiple priority level should be supported
+        lower = -bound[0]
+        wlits = []
+        for l, w in minimize[0]:
+            if w > 0:
+                lower += w
+                l = -l
+            else:
+                w = -w
+            wlits.append((l, w))
+        backend.add_weight_rule([], lower, wlits)
 
     def _optimize(self, control: Control):
         obs = MinObs()
         control.register_observer(obs)
 
-        heu = RestoreHeu()
-        control.register_propagator(heu)
+        heu = None
+        on_model = None
+        if self._restore:
+            heu = RestoreHeu()
+            on_model = heu.on_model
+            control.register_propagator(heu)
 
         control.ground([('base', [])])
-        res = cast(SolveResult, control.solve(on_model=heu.on_model))
+        res = cast(SolveResult, control.solve(on_model=on_model))
 
         solve_config = cast(Configuration, control.configuration.solve)
 
         num_models = int(cast(str, solve_config.models))
 
+        minimize = [ x[1] for x in sorted(obs.literals.items(), key=lambda x: x[0]) ]
+
         while (res.satisfiable and not res.interrupted and
-               obs.literals is not None and 'costs' in control.statistics['summary']):
+               minimize and 'costs' in control.statistics['summary']):
             summary = control.statistics['summary']
             if num_models > 0:
                 num_models -= int(summary['models']['optimal'])
@@ -79,17 +115,14 @@ class OptApt(Application):
                     break
                 solve_config.models = num_models
 
-            bounds = control.statistics['summary']['costs']
+            costs = cast(Tuple[int], tuple(int(x) for x in control.statistics['summary']['costs']))
             with control.backend() as backend:
-                assert len(bounds) == 1
-                bound = -int(bounds[0])
-                for _, w in obs.literals:
-                    bound += w
-                backend.add_weight_rule([], bound, [(-l, w) for (l, w) in obs.literals])
+                self._set_upper_bound(backend, minimize, costs)
 
-            heu.set_restore(int(bounds[0]))
+            if heu is not None:
+                heu.set_restore(costs)
 
-            res = cast(SolveResult, control.solve(on_model=heu.on_model))
+            res = cast(SolveResult, control.solve(on_model=on_model))
 
     def main(self, control: Control, files: Sequence[str]):
         for file_ in files:
