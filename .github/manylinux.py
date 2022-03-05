195885dc46258c13388144072a0b9011b6aead3c
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2022-03-05 18:02:47 +0100

    exclude to build for pypy-3.9 for now

--- .github/manylinux.py
+++ .github/manylinux.py
@@ -53,7 +53,7 @@ def compile_wheels(idx):
     '''
     for pybin in glob('/opt/python/*/bin'):
         # Requires Py3.6 or greater - on the docker image 3.5 is cp35-cp35m
-        if "35" not in pybin:
+        if "35" not in pybin and "pypy3.9" not in pybin:
             check_call(['rm', '-rf', '_skbuild'])
             args = [path.join(pybin, 'pip'), 'wheel', '--verbose', '--no-deps', '-w', 'wheelhouse/']
             if idx is not None:
