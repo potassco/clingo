6f4153d8ae8241b9057d9f24939001a7e9a5fa65
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2022-05-16 20:54:48 +0200

    refine deploy script

--- .github/trigger.sh
+++ .github/trigger.sh
@@ -18,38 +18,93 @@ function dispatch() {
       -d "{\"ref\":\"$3\",\"inputs\":{\"wip\":\"$2\"${4:+,$4}}}"
 }
 
-branch=wip
+function usage() {
+    cat <<EOF
+trigger.sh -h
+    show this help
+trigger.sh list
+    list available workflows
+trigger.sh {release|dev} BRANCH
+    deploy release or development packages
+EOF
+}
+
+function check_action() {
+    if [[ "$1" == "$2" ]]; then
+        if (($3 + $4 != $5 )); then
+            if (( $4 == 0 )); then
+                echo "action '${2}' expects no arguments" >&2
+            elif (( $4 == 1 )); then
+                echo "action '${2}' expects 1 argument" >&2
+            else
+                echo "action '${2}' expects ${4} arguments" >&2
+            fi
+            exit 1
+        fi
+        return 0
+    fi
+    return 1
+}
+
+function fail() {
+    echo "unexpected action '$1'" >&2
+    exit 1
+}
+
+while getopts ":h" flag; do
+    case "$flag" in
+        h)
+            usage
+            exit 0
+            ;;
+        :)
+            echo "ERROR: option '-${OPTARG}' expects an argument" >&2
+            exit 1
+            ;;
+        ?)
+            echo "ERROR: invalid option '-${OPTARG}'" >&2
+            exit 1
+            ;;
+    esac
+done
+
+if (( $OPTIND > $# )); then
+    echo "ERROR: no action given" >&2
+    usage
+    exit 1
+fi
+
+action="${@:$OPTIND:1}"
+
+check_action list "$action" $OPTIND 0 $# ||
+check_action release "$action" $OPTIND 1 $# ||
+check_action dev "$action" $OPTIND 1 $# ||
+fail "$action"
+
 wip=true
 
-case $1 in
+case "$action" in
     list)
         list
         ;;
     release)
-        if [[ $# < 2 ]]; then
-            echo "usage: trigger release REF"
-            exit 1
-        fi
+        branch="${@:$OPTIND+1:1}"
         wip=false
-        branch=$2
         # .github/workflows/manylinux.yml
-        dispatch 4811844 $wip $branch '"image":"manylinux2014_ppc64le"'
-        dispatch 4811844 $wip $branch '"image":"manylinux2014_aarch64"'
+        dispatch 4811844 "$wip" "$branch" '"image":"manylinux2014_ppc64le"'
+        dispatch 4811844 "$wip" "$branch" '"image":"manylinux2014_aarch64"'
         ;&
     dev)
+        branch="${@:$OPTIND+1:1}"
         # .github/workflows/manylinux.yml
-        dispatch 4811844 $wip $branch
+        dispatch 4811844 "$wip" "$branch"
         # .github/workflows/pipsource.yml
-        dispatch 4823035 $wip $branch
+        dispatch 4823035 "$wip" "$branch"
         # .github/workflows/ppa-dev.yml
-        dispatch 4881510 $wip $branch
+        dispatch 4881510 "$wip" "$branch"
         # .github/workflows/conda-dev.yml
-        dispatch 4923491 $wip $branch
+        dispatch 4923491 "$wip" "$branch"
         # .github/workflows/pipwinmac-wip.yml
-        dispatch 4978730 $wip $branch
-        ;;
-    *)
-        echo "usage: trigger {list,dev,release}"
-        exit 1
+        dispatch 4978730 "$wip" "$branch"
         ;;
 esac
