1dac7ddab0fb8fe146850d49e96663e790bd79fc
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2022-09-16 00:00:07 +0200

    fix version detection

--- .github/adjust_version.py
+++ .github/adjust_version.py
@@ -4,15 +4,10 @@ Script to build binary wheels in manylinux 2014 docker container.
 
 import argparse
 from re import finditer, escape, match, sub, search
-from subprocess import check_output, check_call, CalledProcessError
-from os import unlink, environ, path, mkdir
-from glob import glob
-
-ARCH = check_output(['uname', '-m']).decode().strip()
-print(ARCH)
+from subprocess import check_output
 
 
-def adjust_version(url, source):
+def adjust_version(url):
     '''
     Adjust version in setup.py.
     '''
@@ -32,13 +27,11 @@ def adjust_version(url, source):
     assert version is not None
 
     post = 0
+    if search(r'{}-{}\.tar\.gz'.format(package_regex, escape(version)), pip) is not None:
+        post = 1
+
     for m in finditer(r'{}-{}\.post([0-9]+)\.tar\.gz'.format(package_regex, escape(version)), pip):
-        post = max(post, int(m.group(1)) + (1 if source else 0))
-
-
-    print(f"version: {version}")
-    print(f"post: {post}")
-    return
+        post = max(post, m.group(1) + 1)
 
     with open('setup.py', 'w') as fw:
         if post > 0:
@@ -48,19 +41,19 @@ def adjust_version(url, source):
 
 
 def run():
+    '''
+    Adjust version in setup.py depending on build type.
+    '''
     parser = argparse.ArgumentParser(description='Build source package.')
     parser.add_argument('--release', action='store_true', help='Build release package.')
-    parser.add_argument('--source', action='store_true', help='Adjust version for source package.')
     args = parser.parse_args()
 
     if args.release:
         url = 'https://pypi.org/simple'
-        idx = None
     else:
         url = 'https://test.pypi.org/simple'
-        idx = 'https://test.pypi.org/simple/'
 
-    adjust_version(url, args.source)
+    adjust_version(url)
 
 
 if __name__ == "__main__":
