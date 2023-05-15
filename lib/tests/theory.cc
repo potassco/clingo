c63ccd503d0a88684ef16ae66af78b0becfa15df
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2023-05-15 14:15:28 +0200

    add template for theory tests

---
+++ lib/tests/theory.cc
@@ -0,0 +1,21 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "parser.hh"
+
+#include <parser/theory.hh>
+#include <parser/head_literal.hh>
+
+namespace test {
+
+namespace grammar {
+
+using literal = parse_root<::grammar::head_literal, '.'>;
+
+} // namespace grammar
+
+TEST_CASE("theory atoms") {
+    REQUIRE(parse<grammar::literal>("&p(x,y;z){}<=a") == "&p(x,y;z){}<=a");
+}
+
+} // namespace test
+
