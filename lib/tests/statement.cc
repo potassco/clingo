d3216f9f3b4cd86898bedf9b47f5fd964bac3697
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2023-06-11 14:40:43 +0200

    add tests for remaining statements

--- lib/tests/statement.cc
+++ lib/tests/statement.cc
@@ -115,6 +115,39 @@ TEST_CASE("unpool_statement") {
                                                                              " a(2) :- b(4); b(5)."
                                                                              " a(2) :- b(3); b(6)."
                                                                              " a(2) :- b(4); b(6).]");
+    // minimize
+    REQUIRE(unpool_str(parse_statement("#minimize { (1;2)@(2;3),(4;5): p(1;2) }.")) ==
+            "[#minimize { "
+            "1@2,4: p(1); 1@2,4: p(2); 1@2,5: p(1); 1@2,5: p(2); "
+            "1@3,4: p(1); 1@3,4: p(2); 1@3,5: p(1); 1@3,5: p(2); "
+            "2@2,4: p(1); 2@2,4: p(2); 2@2,5: p(1); 2@2,5: p(2); "
+            "2@3,4: p(1); 2@3,4: p(2); 2@3,5: p(1); 2@3,5: p(2) "
+            "}.]");
+    // weak constraint
+    REQUIRE(unpool_str(parse_statement(":~ p(1;2). [(1;2)@(2;3),(4;5)]"), " ") ==
+            "[ :~ p(1). [1@2,4] :~ p(2). [1@2,4] :~ p(1). [1@2,5] :~ p(2). [1@2,5]"
+            "  :~ p(1). [1@3,4] :~ p(2). [1@3,4] :~ p(1). [1@3,5] :~ p(2). [1@3,5]"
+            "  :~ p(1). [2@2,4] :~ p(2). [2@2,4] :~ p(1). [2@2,5] :~ p(2). [2@2,5]"
+            "  :~ p(1). [2@3,4] :~ p(2). [2@3,4] :~ p(1). [2@3,5] :~ p(2). [2@3,5]]");
+    // show
+    REQUIRE(unpool_str(parse_statement("#show (1;2): p(1;2)."), " ") == "[#show 1: p(1). #show 1: p(2)."
+                                                                        " #show 2: p(1). #show 2: p(2).]");
+    // project
+    REQUIRE(unpool_str(parse_statement("#project q(1;2): p(1;2)."), " ") ==
+            "[#project q(1): p(1). #project q(1): p(2)."
+            " #project q(2): p(1). #project q(2): p(2).]");
+    // edge
+    REQUIRE(unpool_str(parse_statement("#edge ((a;b),(c;d)) : p(1;2)."), " ") ==
+            "[#edge (a,c): p(1). #edge (a,c): p(2)."
+            " #edge (a,d): p(1). #edge (a,d): p(2)."
+            " #edge (b,c): p(1). #edge (b,c): p(2)."
+            " #edge (b,d): p(1). #edge (b,d): p(2).]");
+    REQUIRE(unpool_str(parse_statement("#heuristic p(1;2). [(1;2)@(3;4),(5;6)]"), " ") ==
+            "[#heuristic p(1). [1@3,5] #heuristic p(1). [1@3,6] #heuristic p(1). [1@4,5] #heuristic p(1). [1@4,6]"
+            " #heuristic p(1). [2@3,5] #heuristic p(1). [2@3,6] #heuristic p(1). [2@4,5] #heuristic p(1). [2@4,6]"
+            " #heuristic p(2). [1@3,5] #heuristic p(2). [1@3,6] #heuristic p(2). [1@4,5] #heuristic p(2). [1@4,6]"
+            " #heuristic p(2). [2@3,5] #heuristic p(2). [2@3,6] #heuristic p(2). [2@4,5] #heuristic p(2). [2@4,6]]");
+    REQUIRE(unpool_str(parse_statement("#const x=(1)."), "") == "[#const x=1. [default]]");
 }
 
 } // namespace test
