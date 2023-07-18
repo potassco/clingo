cc5f8825cac2c7cea9104047150fe146c452a46b
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2023-07-18 19:50:01 +0200

    refactor visitor

--- lib/src/input/algo/visit.hh
+++ lib/src/input/algo/visit.hh
@@ -1,84 +1,65 @@
 #pragma once
 
+//! @file
+//! This file contains utilities for visiting generic data structures.
+
 #include <optional>
+#include <tuple>
 #include <variant>
 #include <vector>
 
-#include <util/algorithm.hh>
 #include <util/shared_ptr.hh>
 
 namespace Gringo::Input {
 
-namespace Detail {
-
-// Note: GCC 11 has some problem with forward defined function and auto
-// arguments. Thus, we simply make the functions available via ADL.
-struct some_arg {};
-
-template <class T, class F> using can_visit_t = std::enable_if_t<std::is_invocable_r_v<void, F, T const &>>;
-
-template <class T, class F> using can_not_visit_t = std::enable_if_t<!std::is_invocable_r_v<void, F, T const &>>;
-
-template <class... T, size_t... Indices>
-auto visit_tuple(auto &&fun, std::tuple<T...> const &tup, std::index_sequence<Indices...> indices) {
-    static_cast<void>(indices);
-    (visit_value(some_arg{}, fun, std::get<Indices>(tup)), ...);
-}
-
-template <class T> auto visit_value(some_arg arg, auto &&fun, T const &x) -> can_visit_t<T, decltype(fun)> {
-    static_cast<void>(arg);
-    fun(x);
-}
-
-template <class T> auto visit_value(some_arg arg, auto &&fun, T const &x) -> can_not_visit_t<T, decltype(fun)> {
-    static_cast<void>(arg);
-    static_cast<void>(x);
-}
-
-template <class T>
-auto visit_value(some_arg arg, auto &&fun, std::optional<T> const &opt)
-    -> can_not_visit_t<std::optional<T>, decltype(fun)> {
-    static_cast<void>(arg);
-    if (opt.has_value()) {
-        visit_value(some_arg{}, fun, opt.value());
+//! A visitor that eases visiting standard types.
+template <class T> class Visitor {
+  public:
+    //! Recursively visit the given type.
+    template <class U> void visit(U const &x) const {
+        if constexpr (std::is_invocable_r_v<void, T, U const &>) {
+            static_cast<T const *>(this)->operator()(x);
+        } else {
+            visit_(x);
+        }
     }
-}
 
-template <class T, class U>
-auto visit_value(some_arg arg, auto &&fun, std::pair<T, U> const &pair)
-    -> can_not_visit_t<std::pair<T, U>, decltype(fun)> {
-    static_cast<void>(arg);
-    visit_value(some_arg{}, fun, pair.first);
-    visit_value(some_arg{}, fun, pair.second);
-}
+    //! Recursively visit all of the given arguments.
+    template <class... U> void visit(U... args) const { (visit(args), ...); }
 
-template <class... T>
-auto visit_value(some_arg arg, auto &&fun, std::tuple<T...> const &tup)
-    -> can_not_visit_t<std::tuple<T...>, decltype(fun)> {
-    static_cast<void>(arg);
-    return visit_tuple(fun, tup, std::index_sequence_for<T...>{});
-}
-
-template <class... T>
-auto visit_value(some_arg arg, auto &&fun, std::variant<T...> const &var)
-    -> can_not_visit_t<std::variant<T...>, decltype(fun)> {
-    static_cast<void>(arg);
-    return std::visit([&fun](auto const &elem) { visit_value(some_arg{}, fun, elem); }, var);
-}
-
-template <class T>
-auto visit_value(some_arg arg, auto &&fun, std::vector<T> const &vec)
-    -> can_not_visit_t<std::vector<T>, decltype(fun)> {
-    static_cast<void>(arg);
-    for (auto &elem : vec) {
-        visit_value(some_arg{}, fun, elem);
+  private:
+    template <class U> void visit_(std::optional<U> const &opt) const {
+        if (opt.has_value()) {
+            visit(opt.value());
+        }
     }
-}
 
-} // namespace Detail
+    template <class U> auto visit_(Util::shared_ptr<U> const &ptr) const { visit(*ptr); }
 
-template <class... Args> void visit_rec(auto &&fun, Args const &...args) {
-    (Detail::visit_value(Detail::some_arg{}, std::forward<decltype(fun)>(fun), args), ...);
-}
+    template <class U, class V> auto visit_(std::pair<U, V> const &pair) const {
+        visit(pair.first);
+        visit(pair.second);
+    }
+
+    template <class... U, size_t... I>
+    void visit_(std::tuple<U...> const &tup, std::index_sequence<I...> indices) const {
+        static_cast<void>(indices);
+        (visit(std::get<I>(tup)), ...);
+    }
+
+    template <class... U> void visit_(std::tuple<U...> const &tup) const {
+        return visit_(tup, std::index_sequence_for<U...>{});
+    }
+
+    template <class... U> void visit_(std::variant<U...> const &var) const {
+        return std::visit([this](auto const &elem) { this->visit(elem); }, var);
+    }
+
+    template <class U> void visit_(std::vector<U> const &vec) const {
+        for (auto &elem : vec) {
+            visit(elem);
+        }
+    }
+};
 
 } // namespace Gringo::Input
--- lib/src/input/algo/visit_variables.cc
+++ lib/src/input/algo/visit_variables.cc
@@ -10,7 +10,18 @@ namespace {
 // part can also be hidden and only the more specific functions have to be
 // visible.
 
-struct VisitVariables {
+struct VisitVariables : Visitor<VisitVariables> {
+    VisitVariables(VarVisitFun fun, VariableContext ctx = VariableContext::all) : fun{std::move(fun)}, ctx{ctx} {}
+
+    // ignore
+
+    void operator()(std::monostate x) const { static_cast<void>(x); }
+
+    void operator()(std::string const &x) const { static_cast<void>(x); }
+
+    template <class T, class U = std::enable_if_t<std::is_enum_v<T>>> void operator()(T const &x) const {
+        static_cast<void>(x);
+    }
 
     // terms
 
@@ -20,15 +31,15 @@ struct VisitVariables {
 
     void operator()(TermVariable const &term) const { fun(term.name); }
 
-    void operator()(TermTuple const &term) const { visit_rec(*this, term.pool); }
+    void operator()(TermTuple const &term) const { visit(term.pool); }
 
-    void operator()(TermFunction const &term) const { visit_rec(*this, term.pool); }
+    void operator()(TermFunction const &term) const { visit(term.pool); }
 
-    void operator()(TermAbs const &term) const { visit_rec(*this, term.pool); }
+    void operator()(TermAbs const &term) const { visit(term.pool); }
 
-    void operator()(TermUnary const &term) const { visit_rec(*this, term.rhs); }
+    void operator()(TermUnary const &term) const { visit(term.rhs); }
 
-    void operator()(TermBinary const &term) const { visit_rec(*this, term.lhs, term.rhs); }
+    void operator()(TermBinary const &term) const { visit(term.lhs, term.rhs); }
 
     // theory terms
 
@@ -38,11 +49,11 @@ struct VisitVariables {
 
     void operator()(TheoryTermVariable const &term) const { fun(term.name); }
 
-    void operator()(TheoryTermTuple const &term) const { visit_rec(*this, term.elems); }
+    void operator()(TheoryTermTuple const &term) const { visit(term.elems); }
 
-    void operator()(TheoryTermFunction const &term) const { visit_rec(*this, term.args); }
+    void operator()(TheoryTermFunction const &term) const { visit(term.args); }
 
-    void operator()(TheoryTermUnparsed const &term) const { visit_rec(*this, term.elems); }
+    void operator()(TheoryTermUnparsed const &term) const { visit(term.elems); }
 
     // literals
 
@@ -52,114 +63,110 @@ struct VisitVariables {
 
     void operator()(LiteralRelation const &lit) const { static_cast<void>(lit); }
 
-    void operator()(LiteralSymbolic const &lit) const { visit_rec(*this, lit.term); }
+    void operator()(LiteralSymbolic const &lit) const { visit(lit.term); }
 
     // conditional literal
 
     void operator()(ConditionalLiteral const &cond_lit) const {
         if (ctx == VariableContext::all) {
-            visit_rec(*this, cond_lit.cond);
+            visit(cond_lit.cond);
         }
-        visit_rec(*this, cond_lit.lits);
+        visit(cond_lit.lits);
     }
 
     // aggregate
 
-    void operator()(SetAggregate::Element const &elem) const { visit_rec(*this, elem.lit, elem.cond); }
+    void operator()(SetAggregate::Element const &elem) const { visit(elem.lit, elem.cond); }
 
     void operator()(SetAggregate const &lit) const {
         if (ctx == VariableContext::all) {
-            visit_rec(*this, lit.elems);
+            visit(lit.elems);
         }
-        visit_rec(*this, lit.lhs, lit.rhs);
+        visit(lit.lhs, lit.rhs);
     }
 
     // theory
 
     void operator()(TheoryAtom const &atom) const {
         if (ctx == VariableContext::all) {
-            visit_rec(*this, atom.elems);
+            visit(atom.elems);
         }
-        visit_rec(*this, atom.name);
+        visit(atom.name);
     }
 
     // head literal
 
     void operator()(HeadLiteral const &lit) const { std::visit(*this, lit); }
 
-    void operator()(Disjunction const &lit) const { visit_rec(*this, lit.elems); }
+    void operator()(Disjunction const &lit) const { visit(lit.elems); }
 
-    void operator()(HeadAggregate::Element const &elem) const { visit_rec(*this, elem.tuple, elem.lit, elem.cond); }
+    void operator()(HeadAggregate::Element const &elem) const { visit(elem.tuple, elem.lit, elem.cond); }
 
     void operator()(HeadAggregate const &lit) const {
         if (ctx == VariableContext::all) {
-            visit_rec(*this, lit.elems);
+            visit(lit.elems);
         }
-        visit_rec(*this, lit.lhs, lit.rhs);
+        visit(lit.lhs, lit.rhs);
     }
 
-    void operator()(HeadSetAggregate const &lit) const { visit_rec(*this, lit.aggr); }
+    void operator()(HeadSetAggregate const &lit) const { visit(lit.aggr); }
 
-    void operator()(HeadTheoryAtom const &lit) const { visit_rec(*this, lit.atom); }
+    void operator()(HeadTheoryAtom const &lit) const { visit(lit.atom); }
 
     // body literal
 
     void operator()(BodyLiteral const &lit) const { std::visit(*this, lit); }
 
-    void operator()(Conjunction const &lit) const { visit_rec(*this, lit.elems); }
+    void operator()(Conjunction const &lit) const { visit(lit.elems); }
 
-    void operator()(BodyAggregate::Element const &elem) const { visit_rec(*this, elem.tuple, elem.cond); }
+    void operator()(BodyAggregate::Element const &elem) const { visit(elem.tuple, elem.cond); }
 
     void operator()(BodyAggregate const &lit) const {
         if (ctx == VariableContext::all) {
-            visit_rec(*this, lit.elems);
+            visit(lit.elems);
         }
-        visit_rec(*this, lit.lhs, lit.rhs);
+        visit(lit.lhs, lit.rhs);
     }
 
-    void operator()(BodySetAggregate const &lit) const { visit_rec(*this, lit.aggr); }
+    void operator()(BodySetAggregate const &lit) const { visit(lit.aggr); }
 
-    void operator()(BodyTheoryAtom const &lit) const { visit_rec(*this, lit.atom); }
+    void operator()(BodyTheoryAtom const &lit) const { visit(lit.atom); }
 
     // statement
 
     void operator()(Statement const &stm) const { return std::visit(*this, stm); }
 
-    void operator()(Rule const &stm) const { visit_rec(*this, stm.head, stm.body); }
+    void operator()(Rule const &stm) const { visit(stm.head, stm.body); }
 
     void operator()(TheoryDefinition const &stm) const { static_cast<void>(stm); }
 
-    void operator()(StatementOptimize::Tuple const &tuple) const {
-        visit_rec(tuple.weight, tuple.priority, tuple.terms);
-    }
+    void operator()(StatementOptimize::Tuple const &tuple) const { visit(tuple.weight, tuple.priority, tuple.terms); }
 
     void operator()(StatementOptimize const &stm) const {
         if (ctx == VariableContext::all) {
-            visit_rec(*this, stm.elems);
+            visit(stm.elems);
         }
     }
 
-    void operator()(StatementWeakConstraint const &stm) const { visit_rec(*this, stm.body, stm.tuple); }
+    void operator()(StatementWeakConstraint const &stm) const { visit(stm.body, stm.tuple); }
 
-    void operator()(StatementShow const &stm) const { visit_rec(*this, stm.term, stm.body); }
+    void operator()(StatementShow const &stm) const { visit(stm.term, stm.body); }
 
     void operator()(StatementShowSig const &stm) const { static_cast<void>(stm); }
 
-    void operator()(StatementProject const &stm) const { visit_rec(*this, stm.term, stm.body); }
+    void operator()(StatementProject const &stm) const { visit(stm.term, stm.body); }
 
     void operator()(StatementProjectSig const &stm) const { static_cast<void>(stm); }
 
     void operator()(StatementDefined const &stm) const { static_cast<void>(stm); }
 
-    void operator()(StatementExternal const &stm) const { visit_rec(*this, stm.term, stm.body, stm.type); }
+    void operator()(StatementExternal const &stm) const { visit(stm.term, stm.body, stm.type); }
 
-    void operator()(StatementEdge::Edge const &edge) const { visit_rec(*this, edge.u, edge.v); }
+    void operator()(StatementEdge::Edge const &edge) const { visit(edge.u, edge.v); }
 
-    void operator()(StatementEdge const &stm) const { visit_rec(*this, stm.edges, stm.body); }
+    void operator()(StatementEdge const &stm) const { visit(stm.edges, stm.body); }
 
-    void operator()(StatementHeuristic const &stm) const {
-        visit_rec(*this, stm.atom, stm.body, stm.type, stm.prio, stm.mod);
-    }
+    void operator()(StatementHeuristic const &stm) const { visit(stm.atom, stm.body, stm.type, stm.prio, stm.mod); }
 
     void operator()(StatementScript const &stm) const { static_cast<void>(stm); }
 
@@ -170,7 +177,7 @@ struct VisitVariables {
     void operator()(StatementConst const &stm) const { static_cast<void>(stm); }
 
     VarVisitFun fun;
-    VariableContext ctx = VariableContext::all;
+    VariableContext ctx;
 };
 
 } // namespace
@@ -198,7 +205,7 @@ void visit_variables(BodyLiteral const &lit, VarVisitFun fun, VariableContext ct
 }
 
 void visit_variables(StatementOptimize::Element const &elem, VarVisitFun fun) {
-    visit_rec(VisitVariables{std::move(fun)}, elem);
+    VisitVariables{std::move(fun)}.visit(elem);
 }
 
 void visit_variables(Statement const &stm, VarVisitFun fun, VariableContext ctx) {
