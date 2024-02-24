3852bdca461a55464182cf34b1fc64f750d5a844
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2024-02-24 22:03:08 +0100

    better helper for matching types

--- lib/gringo/include/gringo/util/type_traits.hh
+++ lib/gringo/include/gringo/util/type_traits.hh
@@ -6,4 +6,6 @@ namespace Gringo::Util {
 
 template <class S, class... L> inline constexpr bool is_among_v = (std::is_same_v<S, L> || ...);
 
+template <class S, class... L> inline constexpr bool matches = (std::is_same_v<std::remove_cvref_t<S>, L> || ...);
+
 } // namespace Gringo::Util
