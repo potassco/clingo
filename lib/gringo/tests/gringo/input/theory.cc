b6b804b80935b6c9c93f65a59165e19fe29a7b50
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2024-03-02 11:38:16 +0100

    add tests for theory
    these tests fail because of bugs in the parser

---
+++ lib/gringo/tests/gringo/input/theory.cc
@@ -0,0 +1,58 @@
+#include "gringo/input/test.hh"
+
+#include <gringo/input/algo/rewrite_theory.hh>
+
+namespace Gringo::Input::Test {
+
+namespace {
+
+auto rewrite_statement(std::string const &str) -> std::string {
+    ParseHelper ph;
+    auto thy = std::get<StmTheory>(*ph.statement(R"(
+        #theory x {
+            a {
+                -: 1, unary;
+                +: 2, binary, left;
+                -: 3, binary, right;
+                +: 4, unary
+            };
+            b {
+                *: 1, binary, left;
+                /: 2, binary, right
+            };
+            &p/0: a, {<,>}, b, any
+
+        }.)"));
+    TheoryAtomParser parser;
+    parser.add_theory(ph.logger(), thy);
+
+    if (auto stm = ph.statement(str); stm) {
+        if (auto rev = rewrite_theory(ph.logger(), parser, *stm); rev) {
+            return to_str(*rev);
+        }
+        if (ph.logger().has_error()) {
+            // TODO: remove?
+            for (auto &[code, msg] : ph.messages()) {
+                printf("%s\n", msg.c_str());
+            }
+            return "<error>";
+        }
+        return to_str(*stm);
+    }
+    return "<error>";
+}
+
+} // namespace
+
+TEST_CASE("rewrite_theory") {
+    // TODO: priorities and associativity seem to be ignored
+    REQUIRE(rewrite_statement("&p { +x-y }.") == "&p { ((+ x) - y) }.");
+    REQUIRE(rewrite_statement("&p { -x+y }.") == "&p { (- (x + y)) }.");
+    REQUIRE(rewrite_statement("&p { x+y-z }.") == "&p { (x + (y - z)) }.");
+    REQUIRE(rewrite_statement("&p { x-y+z }.") == "&p { ((x - y) + z) }.");
+    REQUIRE(rewrite_statement("&p { x+y+z }.") == "&p { ((x + y) + z) }.");
+    REQUIRE(rewrite_statement("&p { x-y-z }.") == "&p { (x - (y - z)) }.");
+    REQUIRE(rewrite_statement("&p { -x+y+z-a-b-c } < x*y*z/a/b/c.") == "FIXME");
+}
+
+} // namespace Gringo::Input::Test
