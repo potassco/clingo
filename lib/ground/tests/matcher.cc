0ac51b5af508b84ae476a099f34a75733cb816da
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2024-04-25 09:41:42 +0200

    tests for full matcher

--- lib/ground/tests/matcher.cc
+++ lib/ground/tests/matcher.cc
@@ -111,8 +111,56 @@ TEST_CASE("ground_matcher") {
         REQUIRE(!matcher->next(*store, ass));
     }
 
+    SECTION("full matcher") {
+        auto name = store->string("f");
+        auto sym = [&](auto a, auto b) { return store->fun(name, SymbolVec{store->num(a), store->num(b)}, false); };
+        ass = {std::nullopt};
+        auto base = Base{};
+        base.add(sym(1, 1), AtomState::unknown);
+        base.add(sym(2, 2), AtomState::unknown);
+        base.add(sym(1, 3), AtomState::unknown);
+        auto a1 = std::make_unique<TermSymbol>(store->num(1));
+        auto a2 = std::make_unique<TermVariable>(0);
+        auto term = std::make_unique<TermFunction>(name, Util::make_vec<UTerm>(std::move(a1), std::move(a2)));
+        std::vector<bool> const bound = {false};
+        // match all
+        auto m1 = make_atom_matcher(bound, base, *term, MatcherType::all_atoms);
+        m1->init(0);
+        m1->match(*store, ass);
+        base.add(sym(1, 4), AtomState::unknown);
+        base.add(sym(2, 5), AtomState::unknown);
+        REQUIRE(m1->next(*store, ass));
+        REQUIRE(ass[0] == store->num(1));
+        REQUIRE(m1->next(*store, ass));
+        REQUIRE(ass[0] == store->num(3));
+        REQUIRE(!m1->next(*store, ass));
+        m1->init(1);
+        m1->match(*store, ass);
+        REQUIRE(m1->next(*store, ass));
+        REQUIRE(ass[0] == store->num(1));
+        REQUIRE(m1->next(*store, ass));
+        REQUIRE(ass[0] == store->num(3));
+        REQUIRE(m1->next(*store, ass));
+        REQUIRE(ass[0] == store->num(4));
+        REQUIRE(!m1->next(*store, ass));
+        base.add(sym(1, 6), AtomState::unknown);
+        // match old
+        m1 = make_atom_matcher(bound, base, *term, MatcherType::old_atoms);
+        m1->init(1);
+        REQUIRE(m1->next(*store, ass));
+        REQUIRE(ass[0] == store->num(1));
+        REQUIRE(m1->next(*store, ass));
+        REQUIRE(ass[0] == store->num(3));
+        REQUIRE(!m1->next(*store, ass));
+        // match new
+        m1 = make_atom_matcher(bound, base, *term, MatcherType::new_atoms);
+        m1->init(1);
+        m1->match(*store, ass);
+        REQUIRE(m1->next(*store, ass));
+        REQUIRE(ass[0] == store->num(4));
+        REQUIRE(!m1->next(*store, ass));
+    }
     // TODO
-    // - full matcher
     // - hash matcher
 }
 
