557e30a266c821f21c7ebfc57765f79b238c575a
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2024-04-25 15:30:52 +0200

    tests for hash matcher

--- lib/ground/tests/matcher.cc
+++ lib/ground/tests/matcher.cc
@@ -165,20 +165,64 @@ TEST_CASE("ground_matcher") {
         auto sym = [&](auto a, auto b, auto c) {
             return store->fun(name, SymbolVec{store->num(a), store->num(b), store->num(c)}, false);
         };
-        ass = {std::nullopt};
+        ass = {std::nullopt, std::nullopt, std::nullopt};
+        ass[0] = store->num(1);
         auto base = Base{};
-        base.add(sym(1, 1, 1), AtomState::unknown);
-        base.add(sym(2, 2, 2), AtomState::unknown);
-        base.add(sym(1, 3, 3), AtomState::unknown);
-        base.add(sym(2, 4, 4), AtomState::unknown);
+        // join: X=1, f(1,X,Y), f(1,Y,Z)
         auto a1 = std::make_unique<TermSymbol>(store->num(1));
         auto a2 = std::make_unique<TermVariable>(0);
         auto a3 = std::make_unique<TermVariable>(1);
-        auto term =
+        auto b1 = std::make_unique<TermSymbol>(store->num(1));
+        auto b2 = std::make_unique<TermVariable>(1);
+        auto b3 = std::make_unique<TermVariable>(2);
+        auto t1 =
             std::make_unique<TermFunction>(name, Util::make_vec<UTerm>(std::move(a1), std::move(a2), std::move(a3)));
-        std::vector<bool> const bound = {true, false};
-        // TODO: add some tests
-        // - test two matchers simultaneously
+        auto t2 =
+            std::make_unique<TermFunction>(name, Util::make_vec<UTerm>(std::move(b1), std::move(b2), std::move(b3)));
+        std::vector<bool> const v1 = {true, false, false};
+        std::vector<bool> const v2 = {true, true, false};
+        auto m1 = make_atom_matcher(v1, base, *t1, MatcherType::new_atoms);
+        auto m2 = make_atom_matcher(v2, base, *t2, MatcherType::new_atoms);
+        base.add(sym(1, 1, 1), AtomState::unknown);
+        base.add(sym(1, 1, 2), AtomState::unknown);
+        base.add(sym(2, 2, 2), AtomState::unknown);
+        base.add(sym(1, 3, 4), AtomState::unknown);
+        base.add(sym(1, 1, 3), AtomState::unknown);
+        // gen 0
+        m1->init(0);
+        m2->init(0);
+        m1->match(*store, ass);
+        REQUIRE(m1->next(*store, ass));
+        REQUIRE(ass[1] == store->num(1));
+        m2->match(*store, ass);
+        REQUIRE(m2->next(*store, ass));
+        REQUIRE(ass[2] == store->num(1));
+        REQUIRE(m2->next(*store, ass));
+        REQUIRE(ass[2] == store->num(2));
+        REQUIRE(m1->next(*store, ass));
+        REQUIRE(ass[1] == store->num(2));
+        m2->match(*store, ass);
+        REQUIRE(!m2->next(*store, ass));
+        REQUIRE(m1->next(*store, ass));
+        REQUIRE(ass[1] == store->num(3));
+        m2->match(*store, ass);
+        REQUIRE(m2->next(*store, ass));
+        REQUIRE(ass[2] == store->num(4));
+        REQUIRE(!m2->next(*store, ass));
+        REQUIRE(!m1->next(*store, ass));
+        // gen 1
+        base.add(sym(1, 1, 5), AtomState::unknown);
+        base.add(sym(1, 5, 6), AtomState::unknown);
+        m1->init(1);
+        m2->init(1);
+        m2->match(*store, ass);
+        REQUIRE(m1->next(*store, ass));
+        REQUIRE(ass[1] == store->num(5));
+        m2->match(*store, ass);
+        REQUIRE(m2->next(*store, ass));
+        REQUIRE(ass[2] == store->num(6));
+        REQUIRE(!m2->next(*store, ass));
+        REQUIRE(!m1->next(*store, ass));
     }
 }
 
