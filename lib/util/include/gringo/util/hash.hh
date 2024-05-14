5b1f92724e6d7e4a5be6f0bdd5590f7d168a4a00
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2024-05-14 13:30:50 +0200

    slightly improve hash functions
    The commit should somewhat reduce the number of calls to hash_combine.

--- lib/util/include/gringo/util/hash.hh
+++ lib/util/include/gringo/util/hash.hh
@@ -70,6 +70,15 @@ template <class T, class A> auto value_hash(std::vector<T, A> const &x) -> size_
 //! Compute the hash of an immutable array.
 template <class T> auto value_hash(Util::immutable_array<T> const &x) -> size_t;
 
+//! Like value_hash but sometimes faster.
+//!
+//! The resulting hash is intended as an argument for hash_combine and not to
+//! be directly used in a container.
+template <class T> auto value_hash_fast(T const &x) -> size_t;
+
+//! Compute the hash for a given range of elements.
+template <class T> auto value_hash_range(T const &x) -> size_t;
+
 //! Compute and combine the hashes of the given arguments.
 template <class T, class... Args> auto value_hash_record(Args const &...x) -> size_t;
 
@@ -130,6 +139,28 @@ struct value_equal_to {
 
 // Implementation of value_hash
 
+template <class T> auto value_hash_fast(T const &x) -> size_t {
+    if constexpr (std::is_same_v<T, std::type_info>) {
+        return x.hash_code();
+    } else if constexpr (std::is_arithmetic_v<T> || std::is_enum_v<T>) {
+        return std::hash<T>{}(x);
+    } else {
+        return value_hash(x);
+    }
+}
+
+template <class T> auto value_hash_range(T const &x) -> size_t {
+    using std::begin;
+    using std::end;
+    auto ib = std::begin(x);
+    auto ie = std::end(x);
+    if (ib == ie) {
+        return value_hash(typeid(T));
+    }
+    return std::accumulate(ib, ie, value_hash_fast(typeid(T)),
+                           [](auto const &seed, auto const &x) { return hash_combine({seed, value_hash_fast(x)}); });
+}
+
 inline auto value_hash(std::type_info const &x) -> size_t { return hash_mix(x.hash_code()); }
 
 template <class T> auto value_hash(T const &x) -> size_t {
@@ -144,68 +175,65 @@ template <class T> auto value_hash(T const &x) -> size_t {
 
 template <class T> auto value_hash(T *x) -> size_t {
     if (x != nullptr) {
-        return hash_combine({typeid(T *).hash_code(), value_hash(*x)});
+        return hash_combine({value_hash_fast(typeid(T *)), value_hash_fast(*x)});
     }
     return value_hash(typeid(T *));
 }
 
 template <class T> auto value_hash(std::optional<T> const &x) -> size_t {
     if (x) {
-        return hash_combine({typeid(std::optional<T>).hash_code(), value_hash(*x)});
+        return hash_combine({value_hash_fast(typeid(std::optional<T>)), value_hash_fast(*x)});
     }
     return value_hash(typeid(std::optional<T>));
 }
 
 template <class T> auto value_hash(std::reference_wrapper<T> const &x) -> size_t {
-    return hash_combine({typeid(std::reference_wrapper<T>).hash_code(), value_hash(x.get())});
+    return hash_combine({value_hash_fast(typeid(std::reference_wrapper<T>)), value_hash_fast(x.get())});
 }
 
 template <class T, class D> auto value_hash(std::unique_ptr<T, D> const &x) -> size_t {
     if (x) {
-        return hash_combine({typeid(std::unique_ptr<T, D>).hash_code(), value_hash(*x)});
+        return hash_combine({value_hash_fast(typeid(std::unique_ptr<T, D>)), value_hash_fast(*x)});
     }
     return value_hash(typeid(std::unique_ptr<T, D>));
 }
 
 template <class T> auto value_hash(immutable_value<T> const &x) -> size_t {
     if (x) {
-        return hash_combine({typeid(immutable_value<T>).hash_code(), value_hash(*x)});
+        return hash_combine({value_hash_fast(typeid(immutable_value<T>)), value_hash_fast(*x)});
     }
     return value_hash(typeid(immutable_value<T>));
 }
 
 template <class T, class U> auto value_hash(std::pair<T, U> const &x) -> size_t {
-    return hash_combine({typeid(std::pair<T, U>).hash_code(), value_hash(x.first), value_hash(x.second)});
+    return hash_combine(
+        {value_hash_fast(typeid(std::pair<T, U>)), value_hash_fast(x.first), value_hash_fast(x.second)});
 }
 
 template <class... T> auto value_hash(std::tuple<T...> const &x) -> size_t {
     return [&x]<size_t... Indices>([[maybe_unused]] std::index_sequence<Indices...> indices) -> size_t {
-        return hash_combine({typeid(std::tuple<T...>).hash_code(), value_hash(std::get<Indices>(x))...});
+        return hash_combine({value_hash_fast(typeid(std::tuple<T...>)), value_hash_fast(std::get<Indices>(x))...});
     }(std::index_sequence_for<T...>{});
 }
 
 template <class... T> auto value_hash(std::variant<T...> const &x) -> size_t {
     return std::visit(
-        [](auto &&arg) { return hash_combine({typeid(std::variant<T...>).hash_code(), value_hash(arg)}); }, x);
+        [](auto &&arg) { return hash_combine({value_hash_fast(typeid(std::variant<T...>)), value_hash_fast(arg)}); },
+        x);
 }
 
-template <class T, size_t E> auto value_hash(std::span<T, E> const &x) -> size_t {
-    return std::accumulate(x.begin(), x.end(), value_hash(typeid(std::span<T, E>)),
-                           [](auto const &seed, auto const &x) { return hash_combine({seed, value_hash(x)}); });
-}
+template <class T, size_t E> auto value_hash(std::span<T, E> const &x) -> size_t { return value_hash_range(x); }
 
-template <class T, class A> auto value_hash(std::vector<T, A> const &x) -> size_t {
-    return std::accumulate(x.begin(), x.end(), value_hash(typeid(std::vector<T, A>)),
-                           [](auto const &seed, auto const &x) { return hash_combine({seed, value_hash(x)}); });
-}
+template <class T, class A> auto value_hash(std::vector<T, A> const &x) -> size_t { return value_hash_range(x); }
 
-template <class T> auto value_hash(Util::immutable_array<T> const &x) -> size_t {
-    return std::accumulate(x.begin(), x.end(), value_hash(typeid(Util::immutable_array<T>)),
-                           [](auto const &seed, auto const &x) { return hash_combine({seed, value_hash(x)}); });
-}
+template <class T> auto value_hash(Util::immutable_array<T> const &x) -> size_t { return value_hash_range(x); }
 
 template <class T, class... Args> auto value_hash_record(Args const &...x) -> size_t {
-    return hash_combine({value_hash(typeid(T)), value_hash(x)...});
+    if constexpr (sizeof...(Args) == 0) {
+        return value_hash(typeid(T));
+    } else {
+        return hash_combine({value_hash_fast(typeid(T)), value_hash_fast(x)...});
+    }
 }
 
 static constexpr unsigned int default_neighborhood_size = 62;
