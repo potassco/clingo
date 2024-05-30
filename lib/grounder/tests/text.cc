48164b9c18767541bbc15ef4ae5e6c8a12375ce0
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2024-05-30 22:36:01 +0200

    add some tests for the grounder

---
+++ lib/grounder/tests/text.cc
@@ -0,0 +1,49 @@
+#include <gringo/grounder/grounder.hh>
+
+#include <gringo/output/text.hh>
+
+#include <gringo/util/algorithm.hh>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <sstream>
+
+namespace Gringo::Test {
+
+TEST_CASE("grounder_text") {
+    std::ostringstream oss;
+    auto opts = Input::RewriteOptions{};
+    auto log = Gringo::Logger{};
+    log.set_level(LogLevel::error);
+    auto store = Gringo::make_symbol_store(true, false);
+    auto out = Gringo::Output::make_text_output(oss);
+    Gringo::Grounder grd{log, *store, opts, *out};
+    auto params = Input::ProgramParamVec{{store->string("base"), {}}};
+
+    SECTION("fact") {
+        grd.parse("a.");
+        grd.prepare();
+        REQUIRE(grd.ground(params));
+        REQUIRE(oss.str() == "a.\n");
+    }
+    SECTION("basic") {
+        grd.parse("a. b :- a.");
+        grd.prepare();
+        REQUIRE(grd.ground(params));
+        REQUIRE(oss.str() == "a.\nb.\n");
+    }
+    SECTION("normal") {
+        grd.parse("a :- not b. b :- not a.");
+        grd.prepare();
+        REQUIRE(grd.ground(params));
+        REQUIRE(oss.str() == "b :- not a.\na :- not b.\n");
+    }
+    SECTION("condlit") {
+        grd.parse("b :- b : a. a :- a : b.");
+        grd.prepare();
+        REQUIRE(grd.ground(params));
+        REQUIRE(oss.str() == "b :- b: a.\na :- a: b.\n");
+    }
+}
+
+} // namespace Gringo::Test
