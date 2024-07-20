25eca2f88b5732ef5146a14bd344747df3327b90
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2024-07-21 01:32:05 +0200

    more on term parsing

--- lib/input/src/algo/parse/lexer.cc
+++ lib/input/src/algo/parse/lexer.cc
@@ -5,37 +5,193 @@
 
 namespace Gringo::Input {
 
-enum class TokenType : uint8_t {
-    end,
-    error,
-    num,
-    str,
-    anon,
-    var,
-    id,
-    lpar,
-    rpar,
-    plus,
-    minus,
-    star,
-    slash,
-    bslash,
-    qmark,
-    caret,
-    amp,
-    bar,
+enum class Condition : uint8_t {
+    normal,
 };
 
-static auto lex(LexerState &state) -> TokenType;
+enum class TokenType : uint8_t {
+    amp,
+    anon,
+    bar,
+    bslash,
+    caret,
+    colon,
+    comma,
+    end,
+    error,
+    id,
+    lpar,
+    minus,
+    num,
+    plus,
+    qmark,
+    rpar,
+    sem,
+    slash,
+    star,
+    str,
+    var,
+};
+
+static auto lex(LexerState &state, Condition cond) -> TokenType;
+
+// NOLINTBEGIN(performance-avoid-endl)
+
+class Parser {
+  public:
+    Parser(std::unique_ptr<std::istream> in) : state_{std::move(in)} {}
+
+    auto parse_fun() -> bool {
+        if (!branch(TokenType::lpar)) {
+            return true;
+        }
+        if (branch(TokenType::comma)) {
+            return require(TokenType::rpar);
+        }
+        while (true) {
+            if (branch(TokenType::rpar)) {
+                return true;
+            }
+            if (!parse_atomic_term()) {
+                return false;
+            }
+            if (!branch(TokenType::comma)) {
+                return require(TokenType::rpar);
+            }
+        }
+    }
+
+    auto parse_atomic_term() -> bool {
+        if (branch(TokenType::num)) {
+            return true;
+        }
+        if (branch(TokenType::str)) {
+            return true;
+        }
+        if (branch(TokenType::anon)) {
+            return true;
+        }
+        if (branch(TokenType::var)) {
+            return true;
+        }
+        if (branch(TokenType::id)) {
+            return parse_fun();
+        }
+        // TODO: tuples
+        if (branch(TokenType::lpar)) {
+            return parse_term() && require(TokenType::rpar);
+        }
+        return false;
+    }
+
+    auto parse_term() -> bool {
+        // TODO
+        return parse_atomic_term();
+    }
+
+    auto branch(TokenType token) -> bool {
+        if (token_ == token) {
+            consume();
+            return true;
+        }
+        return false;
+    }
+
+    auto require(TokenType token) -> bool {
+        auto ret = token_ == token;
+        consume();
+        return ret;
+    }
+
+    void consume() { token_ = lex(state_, Condition::normal); }
+
+    void parse() {
+        // stack:
+        // function: name args
+        //
+        consume();
+        if (parse2() && require(TokenType::end)) {
+            std::cerr << "parsing successful" << std::endl;
+        } else {
+            std::cerr << "parsing failed" << std::endl;
+        }
+    }
+
+    auto parse2() -> bool {
+        stack_.emplace_back(Prod::term);
+        while (!stack_.empty()) {
+            switch (stack_.back()) {
+                case Prod::term: {
+                    if (branch(TokenType::num)) {
+                        stack_.pop_back();
+                        continue;
+                    }
+                    if (branch(TokenType::str)) {
+                        stack_.pop_back();
+                        continue;
+                    }
+                    if (branch(TokenType::anon)) {
+                        stack_.pop_back();
+                        continue;
+                    }
+                    if (branch(TokenType::var)) {
+                        stack_.pop_back();
+                        continue;
+                    }
+                    if (branch(TokenType::id)) {
+                        if (branch(TokenType::lpar)) {
+                            while (branch(TokenType::sem)) {
+                            }
+                            if (!branch(TokenType::rpar)) {
+                                stack_.back() = Prod::fun;
+                                stack_.push_back(Prod::term);
+                                continue;
+                            }
+                        }
+                        stack_.pop_back();
+                        continue;
+                    }
+                    return false;
+                }
+                case Prod::fun: {
+                    if (branch(TokenType::rpar)) {
+                        stack_.pop_back();
+                        continue;
+                    }
+                    if (branch(TokenType::comma)) {
+                        stack_.push_back(Prod::term);
+                        continue;
+                    }
+                    if (branch(TokenType::sem)) {
+                        while (branch(TokenType::sem)) {
+                        }
+                        if (branch(TokenType::rpar)) {
+                            stack_.pop_back();
+                            continue;
+                        }
+                        stack_.push_back(Prod::term);
+                        continue;
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+  private:
+    enum class Prod : uint8_t { term, fun };
+    LexerState state_;
+    std::vector<Prod> stack_;
+    TokenType token_ = TokenType::error;
+};
+
+// NOLINTEND(performance-avoid-endl)
 
 void test() {
-    std::istringstream iss(R"(425 _abc _ _ABC "\n1x'_:" #xxx)");
-    auto state = LexerState{std::make_unique<std::istringstream>(std::move(iss))};
-    // NOLINTBEGIN(performance-avoid-endl)
-    for (auto token = lex(state); token != TokenType::end; token = lex(state)) {
-        std::cerr << "token: " << static_cast<int>(token) << std::endl;
-    }
-    // NOLINTEND(performance-avoid-endl)
+    std::istringstream iss(R"(f("a", _, X, 1, g(;f,x;;g;)))");
+    auto parser = Parser{std::make_unique<std::istringstream>(std::move(iss))};
+    parser.parse();
 }
 
 #include "algo/parse/lexer_impl.hh"
--- lib/input/src/algo/parse/lexer_state.hh
+++ lib/input/src/algo/parse/lexer_state.hh
@@ -46,8 +46,6 @@ class LexerState {
     auto ctxmarker() -> char *& { return ctxmarker_; }
     //! Pointer marking the end of the input.
     auto limit() -> char *& { return limit_; }
-    //! State variable to capture start conditions.
-    auto condition() -> int & { return condition_; }
     //! Fill the input buffer discarding input before the position marked with start.
     //!
     //! Reallocates if no characters can be discarded
@@ -63,7 +61,6 @@ class LexerState {
     char *ctxmarker_ = nullptr;
     char *token_ = nullptr;
     char *limit_ = nullptr;
-    int condition_ = 0;
     bool eof_ = false;
 };
 
