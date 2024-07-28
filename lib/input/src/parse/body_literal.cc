04b96ba88d46d2d5eb9879e0555e069c735fd294
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2024-07-28 20:37:09 +0200

    start to write parser for body literals

--- lib/input/src/parse/body_literal.cc
+++ lib/input/src/parse/body_literal.cc
@@ -9,9 +9,33 @@ namespace Gringo::Input::Parse {
 namespace {}
 
 auto parse_body_literal(ParserState &state) -> std::optional<BdLit> {
-    static_cast<void>(state);
-    throw std::runtime_error("implement me!!!");
-    return std::nullopt;
+    auto pos = state.token_pos();
+
+    auto sign = Sign::none;
+    if (state.branch(TokenType::not_)) {
+        sign = Sign::once;
+    }
+    if (state.branch(TokenType::not_)) {
+        sign = Sign::twice;
+    }
+
+    static_cast<void>(sign);
+    static_cast<void>(pos);
+
+    // handle atoms or guards of aggregates
+    if (check_term(state.token())) {
+        throw std::runtime_error("implement me!!!");
+    }
+    // handle theory atoms
+    if (state.token() == TokenType::amp) {
+        if (auto atom = parse_theory_atom(state); atom) {
+            throw std::runtime_error("implement me!!!");
+        }
+    }
+    // handle aggregates
+    // TODO: ...
+
+    return state.expected<std::nullopt>(TokenType::amp, "<aggregate-function>", "<term>");
 }
 
 } // namespace Gringo::Input::Parse
