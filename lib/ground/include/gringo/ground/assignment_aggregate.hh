99c1b69ad3cc55af0d915ac2210a2ff00eca69de
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2024-08-27 18:16:48 +0200

    layout assignment aggregate state

--- lib/ground/include/gringo/ground/assignment_aggregate.hh
+++ lib/ground/include/gringo/ground/assignment_aggregate.hh
@@ -103,4 +103,98 @@ class BaseAssignAggr : public BaseImpl<std::pair<size_t, Symbol>, BaseAssignAggr
     bool single_pass_elems_;
 };
 
+class StateAssignAggr {
+  public:
+    // NOLINTBEGIN
+    struct ElementKey {
+        ElementKey(SymbolStore &store, Assignment &ass, AggregateFunction fun, size_t atom_idx, UTermVec const &tuple,
+                   bool &res);
+
+        auto span() const -> SymbolSpan;
+        auto hash() const -> size_t;
+        friend auto operator==(ElementKey const &a, ElementKey const &b) -> bool;
+
+        // Note that these two could be combined to save a little bit of memory.
+        size_t n;
+        size_t atom_idx;
+        GRINGO_IGNORE_ZERO_SIZED_ARRAY_B
+        Symbol syms[0];
+        GRINGO_IGNORE_ZERO_SIZED_ARRAY_E
+    };
+
+    struct AtomKey {
+        AtomKey(SymbolStore &store, Assignment &ass, VariableVec const &global, GuardVec &guards, bool &res);
+
+        AtomKey(Symbol const *tuple, size_t n);
+        GRINGO_IGNORE_ZERO_SIZED_ARRAY_B
+        Symbol syms[0];
+        GRINGO_IGNORE_ZERO_SIZED_ARRAY_E
+    };
+    // NOLINTEND
+
+    using AtomMap = BaseAssignAggr::AtomMap;
+    using ElementMap = Util::ordered_map<ElementKey *, Util::small_vector<size_t>>;
+
+    //! Initialize an aggregate state.
+    StateAssignAggr(VariableVec global, UTerm term, AggregateFunction fun, size_t index, bool single_pass_elems)
+        : base_{global.size(), single_pass_elems}, global_{std::move(global)}, term_{std::move(term)}, index_{index},
+          fun_{fun} {}
+
+    //! Get the global variables in the aggregate.
+    //!
+    //! This does not include the guard of the aggregate.
+    [[nodiscard]] auto global() const -> VariableVec const &;
+    //! Get the target term to assign values to.
+    [[nodiscard]] auto term() const -> Term const &;
+    //! Get the aggregate function.
+    [[nodiscard]] auto fun() const -> AggregateFunction;
+    //! Indicates that all necessary elemements can be grounded in a single
+    //! pass.
+    //!
+    //! This does not take into account the body prefix of elements.
+    [[nodiscard]] auto single_pass_elems() const -> bool;
+    //! Get the update index.
+    [[nodiscard]] auto index() const -> size_t;
+
+    //! Propagate equeued aggregates.
+    auto propagate() -> bool;
+
+    //! Enqueue an atom for propgation.
+    void enqueue(AtomMap::iterator it);
+
+    //! Insert an aggregate atom (stemming from an aggregate element).
+    //!
+    //! This function also enqueues freshly inserted atoms to cover the case
+    //! that the aggregate matches the empty element set.
+    auto insert_atom(SymbolStore &store, Assignment &ass) -> std::optional<AtomMap::iterator>;
+
+    //! Insert an aggregate element.
+    void insert_elem(SymbolStore &store, Assignment &ass, AtomMap::iterator it, UTermVec const &tuple,
+                     auto const &get_cond);
+
+    //! Get the index of an aggregate atom.
+    auto index(AtomMap::iterator it) -> size_t;
+
+    //! Get the index of an aggregate element.
+    auto index(ElementMap::iterator it) -> size_t;
+
+    //! Print a non-ground representation of the aggregate.
+    void print(std::ostream &out);
+
+    //! Get the underlying atom base.
+    [[nodiscard]] auto base() -> BaseAssignAggr &;
+
+    //! Output all previously grounded aggregates.
+    void output(OutputStm &out);
+
+  private:
+    Util::NodeStore<alignof(Symbol)> node_store_;
+    BaseAssignAggr base_;
+    ElementMap tuples_;
+    VariableVec global_;
+    UTerm term_;
+    std::vector<size_t> queue_;
+    size_t index_;
+    AggregateFunction fun_;
+};
 } // namespace Gringo::Ground
