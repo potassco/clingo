39d133702e481f625550e395ce592b981c88163e
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2024-10-23 23:17:37 +0200

    add example

---
+++ scratch/test21.py
@@ -0,0 +1,31 @@
+"""
+Example program adding single parsed statement to program.
+"""
+
+import clingo
+
+
+def main(lib):
+    """
+    Run the main function.
+    """
+    ctl = clingo.control.Control(lib, [])
+    ctl.parse_string("a.")
+
+    prg = clingo.ast.Program(lib)
+    prg.add(clingo.ast.parse_statement(lib, "b :- a."))
+    ctl.join(prg)
+
+    ctl.ground([("base", [])])
+
+
+def run():
+    """
+    Setup library object and run main.
+    """
+    lib = clingo.core.Library()
+    main(lib)
+
+
+if __name__ == "__main__":
+    run()
