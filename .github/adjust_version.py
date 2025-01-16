2112299e6bbf02e18f30a75c6d185fc77ae4f1d1
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-01-16 14:23:12 +0100

    publish python 3.13 pypi packages

--- .github/adjust_version.py
+++ .github/adjust_version.py
@@ -1,6 +1,6 @@
-'''
+"""
 Script to build binary wheels in manylinux 2014 docker container.
-'''
+"""
 
 import argparse
 from re import finditer, escape, match, sub, search
@@ -9,29 +9,29 @@ from os.path import exists
 
 
 def adjust_version(url):
-    '''
+    """
     Adjust version in setup.py.
-    '''
-    if exists('setup.cfg'):
-        setup_type = 'cfg'
-        setup_path = 'setup.cfg'
+    """
+    if exists("setup.cfg"):
+        setup_type = "cfg"
+        setup_path = "setup.cfg"
     else:
-        setup_type = 'py'
-        setup_path = 'setup.py'
+        setup_type = "py"
+        setup_path = "setup.py"
 
     with open(setup_path) as fr:
         setup = fr.read()
 
-    if setup_type == 'cfg':
-        package_name = search(r'name[ ]*=[ ]*(.*)[ ]*', setup).group(1)
-        package_regex = package_name.replace('-', '[-_]')
+    if setup_type == "cfg":
+        package_name = search(r"name[ ]*=[ ]*(.*)[ ]*", setup).group(1)
+        package_regex = package_name.replace("-", "[-_]")
     else:
-        package_name = search(r'''name[ ]*=[ ]*['"]([^'"]*)['"]''', setup).group(1)
-        package_regex = package_name.replace('-', '[-_]')
+        package_name = search(r"""name[ ]*=[ ]*['"]([^'"]*)['"]""", setup).group(1)
+        package_regex = package_name.replace("-", "[-_]")
 
-    pip = check_output(['curl', '-sL', '{}/{}'.format(url, package_name)]).decode()
+    pip = check_output(["curl", "-sL", "{}/{}".format(url, package_name)]).decode()
     version = None
-    with open('libclingo/clingo.h') as fh:
+    with open("libclingo/clingo.h") as fh:
         for line in fh:
             m = match(r'#define CLINGO_VERSION "([0-9]+\.[0-9]+\.[0-9]+)"', line)
             if m is not None:
@@ -39,37 +39,63 @@ def adjust_version(url):
     assert version is not None
 
     post = 0
-    if search(r'{}-{}\.tar\.gz'.format(package_regex, escape(version)), pip) is not None:
+    if (
+        search(r"{}-{}\.tar\.gz".format(package_regex, escape(version)), pip)
+        is not None
+    ):
         post = 1
 
-    for m in finditer(r'{}-{}\.post([0-9]+)\.tar\.gz'.format(package_regex, escape(version)), pip):
+    for m in finditer(
+        r"{}-{}\.post([0-9]+)\.tar\.gz".format(package_regex, escape(version)), pip
+    ):
         post = max(post, int(m.group(1)) + 1)
 
-    with open(setup_path, 'w') as fw:
-        if setup_type == 'cfg':
+    version = "5.7.1"
+    post = 0
+
+    with open(setup_path, "w") as fw:
+        if setup_type == "cfg":
             if post > 0:
-                fw.write(sub('version( *)=.*', 'version = {}.post{}'.format(version, post), setup, 1))
+                fw.write(
+                    sub(
+                        "version( *)=.*",
+                        "version = {}.post{}".format(version, post),
+                        setup,
+                        1,
+                    )
+                )
             else:
-                fw.write(sub('version( *)=.*', 'version = {}'.format(version), setup, 1))
+                fw.write(
+                    sub("version( *)=.*", "version = {}".format(version), setup, 1)
+                )
         else:
             if post > 0:
-                fw.write(sub('version( *)=.*', 'version = \'{}.post{}\','.format(version, post), setup, 1))
+                fw.write(
+                    sub(
+                        "version( *)=.*",
+                        "version = '{}.post{}',".format(version, post),
+                        setup,
+                        1,
+                    )
+                )
             else:
-                fw.write(sub('version( *)=.*', 'version = \'{}\','.format(version), setup, 1))
+                fw.write(
+                    sub("version( *)=.*", "version = '{}',".format(version), setup, 1)
+                )
 
 
 def run():
-    '''
+    """
     Adjust version in setup.py depending on build type.
-    '''
-    parser = argparse.ArgumentParser(description='Build source package.')
-    parser.add_argument('--release', action='store_true', help='Build release package.')
+    """
+    parser = argparse.ArgumentParser(description="Build source package.")
+    parser.add_argument("--release", action="store_true", help="Build release package.")
     args = parser.parse_args()
 
     if args.release:
-        url = 'https://pypi.org/simple'
+        url = "https://pypi.org/simple"
     else:
-        url = 'https://test.pypi.org/simple'
+        url = "https://test.pypi.org/simple"
 
     adjust_version(url)
 
