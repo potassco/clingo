6836dd686e7623f59ed6c624d831eafc9be6ede0
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-01-31 14:42:30 +0100

    add a small python example

---
+++ examples/python-api/order.py
@@ -0,0 +1,50 @@
+"""
+Print programs in a more predictable format.
+
+The idea is to be able to apply diffs to programs with small differences.
+"""
+
+import sys
+from functools import singledispatchmethod
+
+from clingo import ast
+from clingo.core import Library
+
+
+class Order:
+    """
+    Class print programs in a more predictable format.
+    """
+
+    def __init__(self):
+        self._lib = Library()
+
+    @singledispatchmethod
+    def _dispatch(self, expr):
+        """
+        Order bodies of statements.
+        """
+        return expr.transform(self._lib, self._dispatch)
+
+    @_dispatch.register
+    def _(self, stm: ast.StatementRule) -> ast.StatementRule:
+        """
+        Order bodies of rules.
+        """
+        return stm.update(self._lib, body=sorted(stm.body))
+
+    def order(self, files: list[str]) -> None:
+        """
+        Parse the statements in the given files and output them in an orderly fashion.
+        """
+        stms = []
+        with ast.Scanner(self._lib, files) as scn:
+            for stm in scn:
+                stms.append(self._dispatch(stm) or stm)
+
+        for stm in sorted(stms):
+            print(str(stm))
+
+
+if __name__ == "__main__":
+    Order().order(sys.argv[1:])
