3de547c833f776bbadaaa71ca56628fe89f1fb14
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-02-08 18:09:27 +0100

    progress config

--- lib/c-api/src/config.cc
+++ lib/c-api/src/config.cc
@@ -1,12 +1,25 @@
 #include <clingo/config.h>
 
+#include <clasp/cli/clasp_options.h>
+
 #include "lib.hh"
+
+auto cpp_cast(clingo_config_t const *config) -> Clasp::Cli::ClaspCliConfig const * {
+    // NOLINTNEXTLINE
+    return reinterpret_cast<Clasp::Cli::ClaspCliConfig const *>(config);
+}
+
+auto cpp_cast(clingo_config_t *config) -> Clasp::Cli::ClaspCliConfig * {
+    // NOLINTNEXTLINE
+    return reinterpret_cast<Clasp::Cli::ClaspCliConfig *>(config);
+}
 
 auto clingo_config_root(clingo_config_t const *config, clingo_id_t *key) -> clingo_result_t {
     CLINGO_TRY {
-        static_cast<void>(config);
-        *key = 0;
-        throw std::logic_error("implement me!!!");
+        if (config == nullptr || key == nullptr) {
+            return clingo_result_invalid;
+        }
+        *key = Clasp::Cli::ClaspCliConfig::key_root;
     }
     CLINGO_CATCH;
 }
@@ -14,10 +27,23 @@ auto clingo_config_root(clingo_config_t const *config, clingo_id_t *key) -> clin
 auto clingo_config_type(clingo_config_t const *config, clingo_id_t key, clingo_config_type_bitset_t *type)
     -> clingo_result_t {
     CLINGO_TRY {
-        static_cast<void>(config);
-        static_cast<void>(key);
+        if (config == nullptr || type == nullptr) {
+            return clingo_result_invalid;
+        }
+        int map_len = 0;
+        int arr_len = 0;
+        int val_len = 0;
+        cpp_cast(config)->getKeyInfo(key, &map_len, &arr_len, nullptr, &val_len);
         *type = 0;
-        throw std::logic_error("implement me!!!");
+        if (map_len >= 0) {
+            *type |= clingo_config_type_map;
+        }
+        if (arr_len >= 0) {
+            *type |= clingo_config_type_array;
+        }
+        if (val_len >= 0) {
+            *type |= clingo_config_type_value;
+        }
     }
     CLINGO_CATCH;
 }
@@ -25,20 +51,25 @@ auto clingo_config_type(clingo_config_t const *config, clingo_id_t key, clingo_c
 auto clingo_config_description(clingo_config_t const *config, clingo_id_t key, char const **description)
     -> clingo_result_t {
     CLINGO_TRY {
-        static_cast<void>(config);
-        static_cast<void>(key);
-        *description = nullptr;
-        throw std::logic_error("implement me!!!");
+        if (config == nullptr || description == nullptr) {
+            return clingo_result_invalid;
+        }
+        cpp_cast(config)->getKeyInfo(key, nullptr, nullptr, description, nullptr);
     }
     CLINGO_CATCH;
 }
 
 auto clingo_config_array_size(clingo_config_t const *config, clingo_id_t key, size_t *size) -> clingo_result_t {
     CLINGO_TRY {
-        static_cast<void>(config);
-        static_cast<void>(key);
-        *size = 0;
-        throw std::logic_error("implement me!!!");
+        if (config == nullptr || size == nullptr) {
+            return clingo_result_invalid;
+        }
+        int len = 0;
+        cpp_cast(config)->getKeyInfo(key, nullptr, &len, nullptr, nullptr);
+        if (len < 0) {
+            return clingo_result_invalid;
+        }
+        *size = static_cast<size_t>(len);
     }
     CLINGO_CATCH;
 }
