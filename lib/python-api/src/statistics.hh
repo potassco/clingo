9de23c44fc68589a582d7b9b8974d2db199ac6ee
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-02-10 20:02:42 +0100

    progress

--- lib/python-api/src/statistics.hh
+++ lib/python-api/src/statistics.hh
@@ -1,8 +1,12 @@
 #pragma once
 
+#include "clingo/statistics.h"
+
 #include <pybind11/pybind11.h>
 
 namespace Clingo::Python {
+
+auto as_dict(clingo_statistics_t *stats) -> pybind11::dict;
 
 void register_statistics(pybind11::module &m);
 
