3765baae09b94290756f6415265397ac987766aa
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-02-24 12:41:13 +0100

    more tests for observer

--- lib/python-api/tests/test_observe.py
+++ lib/python-api/tests/test_observe.py
@@ -4,17 +4,45 @@ Unit tests for clingo.backend module.
 
 from typing import Sequence
 
-from clingo.backend import Observer
+from clingo.backend import ExternalType, HeuristicType, Observer
 from clingo.control import Control
 from clingo.core import Library
+from clingo.symbol import Function
 
 
 class ExampleObserver(Observer):
+    # pylint: disable=missing-function-docstring,redefined-builtin,too-many-instance-attributes
+    """
+    Example observer for testing that tracks function calls.
+    """
+
     def __init__(self) -> None:
         super().__init__()
+        self.assumptions = []
+        self.begin_steps = 0
+        self.edges = []
+        self.end_steps = 0
+        self.externals = []
+        self.heuristics = []
+        self.incremental = None
+        self.minimizes = []
+        self.projections = []
         self.rules = []
         self.weight_rules = []
-        self.projections = []
+
+    def init_program(self, incremental: bool) -> None:
+        if self.incremental is not None:
+            raise RuntimeError("multiple calls to init_program")
+        self.incremental = incremental
+
+    def begin_step(self) -> None:
+        self.begin_steps += 1
+
+    def end_step(self) -> None:
+        self.end_steps += 1
+
+    def assume(self, literals: Sequence[int]) -> None:
+        self.assumptions.append(literals)
 
     def rule(self, head: Sequence[int], body: Sequence[int], choice: bool) -> None:
         self.rules.append((head, body, choice))
@@ -30,6 +58,25 @@ class ExampleObserver(Observer):
 
     def project(self, atoms: Sequence[int]) -> None:
         self.projections.append(atoms)
+
+    def heuristic(
+        self,
+        atom: int,
+        type: HeuristicType,
+        weight: int,
+        priority: int,
+        condition: Sequence[int],
+    ) -> None:
+        self.heuristics.append((atom, type, weight, priority, condition))
+
+    def external(self, atom: int, type: ExternalType) -> None:
+        self.externals.append((atom, type))
+
+    def edge(self, node_u: int, node_v: int, condtition: Sequence[int]) -> None:
+        self.edges.append((node_u, node_v, condtition))
+
+    def minimize(self, literals: Sequence[tuple[int, int]], priority: int) -> None:
+        self.minimizes.append((literals, priority))
 
 
 class TestObserve:
@@ -79,6 +126,8 @@ class TestObserve:
         obs = ExampleObserver()
         self.ctl.observe(obs)
 
+        assert obs.incremental
+        assert obs.begin_steps == obs.end_steps == 1
         assert len(obs.rules) == 2
         assert sum(1 for _, _, choice in obs.rules if choice) == 1
         assert sum(1 for _, _, choice in obs.rules if not choice) == 1
@@ -86,6 +135,7 @@ class TestObserve:
         self.ctl.parse_string("#program b. {p(1..20)}. :- #sum { X: p(X) } >= 40.")
         self.ctl.ground([("b", [])])
         self.ctl.observe(obs)
+        assert obs.begin_steps == obs.end_steps == 2
         assert len(obs.weight_rules) == 1
         assert len(obs.weight_rules[0][0]) == 0
         assert obs.weight_rules[0][1] == 40
@@ -95,5 +145,22 @@ class TestObserve:
         self.ctl.parse_string("#program c. #project p/1.")
         self.ctl.ground([("c", [])])
         self.ctl.observe(obs)
+        assert obs.begin_steps == obs.end_steps == 3
         assert len(obs.projections) == 1
         assert len(obs.projections[0]) == 20
+
+        with self.ctl.backend as bck:
+            a = bck.atom(Function(self.lib, "a"))
+            b = bck.atom(Function(self.lib, "b"))
+            c = bck.atom(Function(self.lib, "c"))
+            bck.assume([a, b, c])
+        self.ctl.observe(obs)
+        assert obs.begin_steps == obs.end_steps == 4
+        assert len(obs.assumptions) == 1
+        assert len(obs.assumptions[0]) == 3
+
+        # TODO:
+        # - externals
+        # - heuristics
+        # - edge
+        # - minimize
