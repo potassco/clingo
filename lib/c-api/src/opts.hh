feca6f6ff922d9e409e483b1554a7f8a468aed14
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-04-23 14:54:19 +0200

    add option to suppress specific warnings

--- lib/c-api/src/opts.hh
+++ lib/c-api/src/opts.hh
@@ -53,14 +53,40 @@ class ClingoOptions {
             return ret.ec == std::errc{} && ret.ptr == end;
         };
         auto parse_level = [this]([[maybe_unused]] std::string const &name, std::string const &value) {
-            auto lvl = LogLevel::info;
-            if (parseValue(values<LogLevel>({{"error", Clingo::LogLevel::error},
-                                             {"warn", Clingo::LogLevel::warn},
-                                             {"info", Clingo::LogLevel::info},
-                                             {"debug", Clingo::LogLevel::debug},
-                                             {"trace", Clingo::LogLevel::trace}}),
-                           value, lvl)) {
+            if (auto lvl = LogLevel::info; parseValue(values<LogLevel>({{"error", LogLevel::error},
+                                                                        {"warn", LogLevel::warn},
+                                                                        {"info", LogLevel::info},
+                                                                        {"debug", LogLevel::debug},
+                                                                        {"trace", LogLevel::trace}}),
+                                                      value, lvl)) {
                 log_->set_level(lvl);
+                return true;
+            }
+            return false;
+        };
+        auto parse_info = [this]([[maybe_unused]] std::string const &name, std::string const &value) {
+            auto vals = values<std::pair<MessageCode, bool>>({
+                {"all", std::pair{MessageCode::info, true}},
+                {"none", std::pair{MessageCode::info, false}},
+                {"operation-undefined", std::pair{MessageCode::info_operation_undefined, true}},
+                {"atom-undefined", std::pair{MessageCode::info_atom_undefined, true}},
+                {"file-included", std::pair{MessageCode::info_file_included, true}},
+                {"global-varibale", std::pair{MessageCode::info_global_variable, true}},
+                {"no-operation-undefined", std::pair{MessageCode::info_operation_undefined, false}},
+                {"no-atom-undefined", std::pair{MessageCode::info_atom_undefined, false}},
+                {"no-file-included", std::pair{MessageCode::info_file_included, false}},
+                {"no-global-varibale", std::pair{MessageCode::info_global_variable, false}},
+            });
+            if (auto val = std::pair{MessageCode::info, true}; parseValue(vals, value, val)) {
+                if (val.first == MessageCode::info) {
+                    for (auto const &[k, v] : vals) {
+                        if (v.second == val.second && v.first != val.first) {
+                            log_->enable(v.first, v.second);
+                        }
+                    }
+                } else {
+                    log_->enable(val.first, val.second);
+                }
                 return true;
             }
             return false;
@@ -93,18 +119,16 @@ class ClingoOptions {
         root.add(group_grounder);
 
         auto group_basic = OptionGroup{"Basic Options"};
-        group_basic.addOptions()                                                                 //
-            ("single-shot", flag(solver_opts_.single_shot = false), "Force single shot solving") //
-            ("log-level,@1", parse(parse_level), "Select log level {error|warn|info|debug|trace}");
-        ;
-        // TODO: warning options
-        // - it's a bit odd to do this here because it affects the lib,
-        //   which is created beforehand
-        // - a clean alternative would require two main functions
-        //   - the existing one could stay as is
-        //   - an additional one would have to create a lib
-        // - probably it is a reasonable enough choice to reconfigure the lib here
-        //   no defaults should be applied though
+        group_basic.addOptions()                                                                   //
+            ("single-shot", flag(solver_opts_.single_shot = false), "Force single shot solving")   //
+            ("log-level,@1", parse(parse_level), "Select log level {error|warn|info|debug|trace}") //
+            ("info,W,@1", parse(parse_info), R"(Enable/disable specific info messages:
+      none                    : disable all
+      all                     : enable all
+      [no-]atom-undefined     : a :- b.
+      [no-]file-included      : #include "a.lp". #include "a.lp".
+      [no-]operation-undefined: p(1/0).
+      [no-]global-variable    : :- #count { X } = 1, X = 1.)");
         root.add(group_basic);
     }
 
