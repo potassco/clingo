643aaf87236b0ce9c4c9841b6128fa035c2fedbb
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-04-28 10:29:26 +0200

    add cxx lib structure

---
+++ lib/cxx-api/tests/symbol.cc
@@ -0,0 +1,14 @@
+#include <clingo/symbol.hh>
+
+#include <catch2/catch_test_macros.hpp>
+
+// NOLINTBEGIN(readability-magic-numbers)
+
+namespace Clingo::Test {
+
+TEST_CASE("symbol") {
+}
+
+} // namespace Clingo::Test
+
+// NOLINTEND(readability-magic-numbers)
