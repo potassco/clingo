4f99bc9505320b9b14ac620a0e7c454adfa5b140
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-05-25 12:40:46 +0200

    more on theory

--- lib/cxx-api/include/clingo/theory.hh
+++ lib/cxx-api/include/clingo/theory.hh
@@ -16,12 +16,12 @@ class TheoryAssignment {
     class iterator {
       public:
         using iterator_category = std::input_iterator_tag;
-        using value_type = std::pair<Clingo::Symbol, std::variant<int, double, Clingo::Symbol>>;
+        using value_type = std::pair<Symbol, std::variant<int, double, Symbol>>;
         using difference_type = std::ptrdiff_t;
         using pointer = value_type *;
         using reference = value_type &;
 
-        iterator(clingo_theory_t *theory, uint32_t thread_id) : theory_{theory}, thread_id_{thread_id} { advance(); }
+        iterator(TheoryAssignment const &assignment) : assignment_{&assignment} { advance(); }
 
         iterator() = default;
 
@@ -38,57 +38,71 @@ class TheoryAssignment {
 
       private:
         void advance() {
-            Clingo::Detail::handle_error(
-                theory_->assignment_next(theory_->self, thread_id_, &init_, &index_, &has_value_));
+            Detail::handle_error(assignment_->theory_->assignment_next(
+                assignment_->theory_->self, assignment_->thread_id_, &init_, &index_, &has_value_));
             if (has_value_) {
-                clingo_symbol_t symbol = 0;
-                clingo_theory_value_t value;
-                Clingo::Detail::handle_error(
-                    theory_->assignment_get_value(theory_->self, thread_id_, index_, &symbol, &value, nullptr));
-                current_.first = Clingo::Symbol{symbol, false};
-                switch (value.type) {
-                    case clingo_theory_value_type_int: {
-                        current_.second = value.int_number; // NOLINT
-                        break;
-                    }
-                    case clingo_theory_value_type_double: {
-                        current_.second = value.double_number; // NOLINT
-                        break;
-                    }
-                    case clingo_theory_value_type_symbol: {
-                        current_.second = Clingo::Symbol{value.symbol, false}; // NOLINT
-                        break;
-                    }
-                    default: {
-                        throw std::logic_error{"invalid symbol type"};
-                    }
-                }
+                current_ = assignment_->at(index_);
             }
         }
 
         mutable value_type current_;
-        clingo_theory_t *theory_ = nullptr;
+        TheoryAssignment const *assignment_ = nullptr;
         size_t index_ = 0;
-        uint32_t thread_id_ = 0;
         bool init_ = true;
         bool has_value_ = true;
     };
     static_assert(std::input_iterator<iterator>);
     static_assert(std::sentinel_for<sentinel, iterator>);
 
-    explicit TheoryAssignment(clingo_theory_t *theory, uint32_t thread_id) : theory_{theory}, thread_id_{thread_id} {}
+    explicit TheoryAssignment(clingo_theory_t const *theory, uint32_t thread_id)
+        : theory_{theory}, thread_id_{thread_id} {}
 
-    [[nodiscard]] auto begin() const -> iterator { return {theory_, thread_id_}; }
+    [[nodiscard]] auto lookup(Symbol const &sym) const -> std::optional<size_t> {
+        bool found = false;
+        size_t index = 0;
+        theory_->lookup_symbol(theory_->self, c_cast(sym), &index, &found);
+        return found ? std::optional{index} : std::nullopt;
+    }
+
+    [[nodiscard]] auto at(size_t index) const -> std::pair<Symbol, std::variant<int, double, Symbol>> {
+        clingo_symbol_t symbol = 0;
+        clingo_theory_value_t value;
+        bool found = false;
+        Detail::handle_error(theory_->assignment_get_value(theory_->self, thread_id_, index, &symbol, &value, &found));
+        auto sym = Clingo::Symbol{symbol, false};
+        if (!found) {
+            throw std::out_of_range{"invalid index"};
+        }
+        switch (value.type) {
+            case clingo_theory_value_type_int: {
+                return {std::move(sym), value.int_number}; // NOLINT
+                break;
+            }
+            case clingo_theory_value_type_double: {
+                return {std::move(sym), value.double_number}; // NOLINT
+                break;
+            }
+            case clingo_theory_value_type_symbol: {
+                return {std::move(sym), Symbol{value.symbol, false}}; // NOLINT
+                break;
+            }
+            default: {
+                throw std::logic_error{"invalid symbol type"};
+            }
+        }
+    }
+
+    [[nodiscard]] auto begin() const -> iterator { return {*this}; }
     [[nodiscard]] static auto end() -> sentinel { return {}; }
 
   private:
-    clingo_theory_t *theory_;
+    clingo_theory_t const *theory_;
     uint32_t thread_id_;
 };
 
 class Theory {
   public:
-    Theory(Clingo::Library const &lib, bool (*create)(clingo_lib_t *lib, clingo_theory_t *theory)) {
+    Theory(Library const &lib, bool (*create)(clingo_lib_t *lib, clingo_theory_t *theory)) {
         create(c_cast(lib), &theory_);
     }
     Theory(Theory &&other) = delete;
@@ -98,52 +112,59 @@ class Theory {
         }
     }
 
-    void prepare(Clingo::Control const &ctl) const {
-        Clingo::Detail::handle_error(theory_.prepare(theory_.self, c_cast(ctl)));
-    }
+    void prepare(Control const &ctl) const { Detail::handle_error(theory_.prepare(theory_.self, c_cast(ctl))); }
 
-    template <class F> void rewrite(Clingo::AST::Node const &stm, F fun) const {
+    template <class F> void rewrite(AST::Node const &stm, F fun) const {
         constexpr auto add = [](clingo_ast_t *stm, void *data) -> bool {
             CLINGO_TRY {
                 auto *fun = static_cast<F *>(data);
-                std::invoke<F &>(*fun, Clingo::AST::Node{stm, true});
+                std::invoke<F &>(*fun, AST::Node{stm, true});
             }
             CLINGO_CATCH;
         };
-        Clingo::Detail::handle_error(theory_.rewrite_ast(theory_.self, c_cast(stm), +add, static_cast<void *>(&fun)));
+        Detail::handle_error(theory_.rewrite_ast(theory_.self, c_cast(stm), +add, static_cast<void *>(&fun)));
     }
 
-    void rewrite(Clingo::Library const &lib, Clingo::Control const &ctl, std::string_view str) const {
-        auto scanner = Clingo::AST::Scanner{lib, str};
-        auto prg = Clingo::AST::Program{lib};
+    void rewrite(Library const &lib, Control const &ctl, std::string_view str) const {
+        auto scanner = AST::Scanner{lib, str};
+        auto prg = AST::Program{lib};
         for (auto const &stm : scanner) {
-            rewrite(stm, [&](Clingo::AST::Node const &node) { prg.add(node); });
+            rewrite(stm, [&](AST::Node const &node) { prg.add(node); });
         }
         ctl.join(prg);
     }
 
-    void rewrite(Clingo::Library const &lib, Clingo::Control const &ctl, Clingo::StringSpan files) const {
-        auto scanner = Clingo::AST::Scanner{lib, files};
-        auto prg = Clingo::AST::Program{lib};
+    void rewrite(Library const &lib, Control const &ctl, StringSpan files) const {
+        auto scanner = AST::Scanner{lib, files};
+        auto prg = AST::Program{lib};
         for (auto const &stm : scanner) {
-            rewrite(stm, [&](Clingo::AST::Node const &node) { prg.add(node); });
+            rewrite(stm, [&](AST::Node const &node) { prg.add(node); });
         }
         ctl.join(prg);
     }
 
-    auto assignment(uint32_t thread_id) -> TheoryAssignment { return TheoryAssignment{&theory_, thread_id}; }
-
-    void stats(Clingo::Stats step, [[maybe_unused]] Clingo::Stats accu) const {
-        Clingo::Detail::handle_error(theory_.on_stats(theory_.self, c_cast(step)));
+    [[nodiscard]] auto assignment(uint32_t thread_id) const -> TheoryAssignment {
+        return TheoryAssignment{&theory_, thread_id};
     }
 
-    void register_theory(Clingo::Control const &ctl) const {
-        Clingo::Detail::handle_error(theory_.register_theory(theory_.self, c_cast(ctl)));
+    void stats(Stats step, [[maybe_unused]] Stats accu) const {
+        Detail::handle_error(theory_.on_stats(theory_.self, c_cast(step)));
     }
+
+    void model(Model model) const { Detail::handle_error(theory_.on_model(theory_.self, c_cast(model))); }
+
+    void register_theory(Control const &ctl) const {
+        Detail::handle_error(theory_.register_theory(theory_.self, c_cast(ctl)));
+    }
+
+    void register_options(Options const &opts) const {
+        Detail::handle_error(theory_.register_options(theory_.self, c_cast(opts)));
+    }
+
+    void validate_options() const { Detail::handle_error(theory_.validate_options(theory_.self)); }
 
     void configure(std::string_view key, std::string_view value) const {
-        Clingo::Detail::handle_error(
-            theory_.configure(theory_.self, key.data(), key.size(), value.data(), value.size()));
+        Detail::handle_error(theory_.configure(theory_.self, key.data(), key.size(), value.data(), value.size()));
     }
 
   private:
