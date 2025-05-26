2d85b365f27c4146067f44706cb0220044d9bdb5
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-05-26 13:47:24 +0200

    update formatting script

--- cmake/glob-paths.py
+++ cmake/glob-paths.py
@@ -4,9 +4,22 @@ import os
 import os.path
 import re
 import sys
+import textwrap
+
+
+def dedent(x):
+    """
+    Patched dedent that also takes the last empty line into account.
+    """
+    return textwrap.dedent(x + "x")[:-1]
 
 
 def split_path(path):
+    """
+    Normalize the given path and return a list of its components.
+
+    A leading '.' of a relative path is removed.
+    """
     components = os.path.normpath(path).split(os.sep)
     if len(components) > 0 and components[0] == ".":
         del components[0]
@@ -14,97 +27,97 @@ def split_path(path):
 
 
 def find(path, target):
+    """
+    Find and replace target sources markers.
+    """
     header = {}
     output = ""
-    for root, dirnames, filenames in os.walk(path):
+    for root, _, filenames in os.walk(path):
         for filename in sorted(filenames):
             components = split_path(root)
             components.append(filename)
             if re.match(r"^.*\.(h|hh|hpp|c|cc|cpp)$", filename):
                 header.setdefault(root, "")
-                header[root] += '    "{src}"\n'.format(src="/".join(components))
+                header[root] += f'    "{"/".join(components)}"\n'
             elif re.match(r"^.*\.(yy)$", filename):
                 name, ext = os.path.splitext(filename)
                 path = "".join([d + "/" for d in components[:-1]])
-                header[os.path.join(root, name)] = (
-                    """\
-    "${path}{name}{ext}"
-    ${{BISON_{name}_OUTPUTS}}
-""".format(
-                        name=name, ext=ext, path=path
-                    )
+                header[os.path.join(root, name)] = dedent(
+                    f"""\
+                    "{path}{name}{ext}"
+                    ${{BISON_{name}_OUTPUTS}}
+                """
                 )
-                output += """\
-bison_target_or_gen("{path}{name}{ext}")
-""".format(
-                    name=name, path=path, ext=ext
-                )
+                output += f'bison_target_or_gen("{path}{name}{ext}")\n'
             elif re.match(r"^.*\.(xh|xch)$", filename):
                 header.setdefault(root, "")
                 name, ext = os.path.splitext(filename)
                 path = "/".join(components[:-1])
-                options = ""
-                if ext == ".xch":
-                    options = " OPTIONS -c"
-                header[
-                    root
-                ] += """\
-    "{path}/{name}{ext}"
-    ${{RE2C_{name}_OUTPUT}}
-""".format(
-                    name=name, ext=ext, path=path
+                header[root] += dedent(
+                    f"""\
+                    "{path}/{name}{ext}"
+                    ${{RE2C_{name}_OUTPUT}}
+                """
                 )
-                output += """\
-re2c_target_or_gen("{path}/{name}{ext}")
-""".format(
-                    name=name, path=path, ext=ext, options=options
-                )
+                output += f're2c_target_or_gen("{path}/{name}{ext}")\n'
 
-    output += 'set(ide_{}_group "{} Files")\n'.format(target, target.title())
+    output += f'set(ide_{target}_group "{target.title()} Files")\n'
 
     groups = []
     for root in sorted(header):
         components = split_path(root)
         if len(components) > 0 and components[0] in ("src", "include", "tests"):
             del components[0]
-        groups.append("-".join(["{}-group".format(target)] + components))
-        output += "set({0}\n".format(groups[-1])
+        groups.append("-".join([f"{target}-group"] + components))
+        output += f"set({groups[-1]}\n"
         for value in header[root]:
             output += value
         output = output[:-1] + ")\n"
-        output += """source_group("{}" FILES ${{{}}})\n""".format(
-            r"\\".join(["${{ide_{}_group}}".format(target)] + components), groups[-1]
-        )
+        target_group = r"\\".join([f"${{ide_{target}_group}}"] + components)
+        output += f'source_group("{target_group}" FILES ${{{groups[-1]}}})\n'
 
-    output += "set({}\n".format(target)
+    output += f"set({target}\n"
     for group in groups:
-        output += """    ${{{}}}\n""".format(group)
+        output += f"    ${{{group}}}\n"
     output = output[:-1] + ")\n"
     return output
 
 
 def rep(m):
+    """
+    Generate file list.
+    """
     path = m.group("path")
     target = m.group("target")
     content = find(path, target)
-    return "# [[[{}: {}\n{}# ]]]".format(target, path, content)
+    return f"# [[[{target}: {path}\n{content}# ]]]"
 
 
-files = [os.path.abspath(f) for f in sys.argv[1:]]
+def run():
+    """
+    Entry point of the script.
+    """
+    files = [os.path.abspath(f) for f in sys.argv[1:]]
 
-for f in files:
-    os.chdir(os.path.dirname(f))
+    for f in files:
+        os.chdir(os.path.dirname(f))
 
-    content = open(f, "r").read()
-    replace = re.sub(
-        r"#[ ]*\[\[\[(?P<target>[^:]*): (?P<path>[^\n\]]*)(.|\n)*?\]\]\]",
-        rep,
-        content,
-        0,
-        re.MULTILINE,
-    )
+        with open(f, "r", encoding="utf8") as hnd:
+            content = hnd.read()
+        replace = re.sub(
+            r"#[ ]*\[\[\[(?P<target>[^:]*): (?P<path>[^\n\]]*)(.|\n)*?\]\]\]",
+            rep,
+            content,
+            0,
+            re.MULTILINE,
+        )
 
-    if content != replace:
-        sys.stderr.write("File {} changed!\n".format(f))
-        sys.stderr.flush()
-        open(f, "w").write(replace)
+        if content != replace:
+            sys.stderr.write(f"File {f} changed!\n")
+            sys.stderr.flush()
+            with open(f, "w", encoding="utf8") as hnd:
+                hnd.write(replace)
+
+
+if __name__ == "__main__":
+    run()
