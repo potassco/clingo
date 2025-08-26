30334e0ddc1deda254433c4bb7481297f06a46ee
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-08-26 10:35:21 +0200

    add tests for python config extension

--- lib/python-api/tests/test_config.py
+++ lib/python-api/tests/test_config.py
@@ -3,10 +3,75 @@ Unit tests for the clingo.config module.
 """
 
 import re
+from textwrap import dedent
 
 from clingo.control import Control
 from clingo.core import Library
 from util import MCB
+
+
+class PluginConfig:
+    """
+    Simple test config class.
+    """
+
+    def __init__(self):
+        self.value = "default value"
+        self.array = ["default value"]
+        self.group = "some group"
+
+    def set_val(self, value):
+        """
+        Set the value of the value config.
+        """
+        self.value = value
+
+    def get_val(self):
+        """
+        Get the value of the value config.
+        """
+        return self.value
+
+    def set_arr_len(self, value):
+        """
+        Set the length of the config array.
+        """
+        while len(self.array) < int(value):
+            self.array.append("default value")
+
+    def get_arr_len(self):
+        """
+        Get the length of the config array.
+        """
+        return len(self.array)
+
+    def set_arr_val(self, value, index=None):
+        """
+        Set the array value at index to value.
+        """
+        if index is None:
+            index = 0
+        self.array[index] = value
+
+    def get_arr_val(self, index=None):
+        """
+        Get the value at index of the config array.
+        """
+        if index is None or index >= len(self.array):
+            return "undefined"
+        return self.array[index]
+
+    def get_group(self):
+        """
+        Set the value of the group config.
+        """
+        return self.group
+
+    def set_group(self, value):
+        """
+        Get the value of the group config.
+        """
+        self.group = value
 
 
 class TestConfig:
@@ -85,3 +150,47 @@ class TestConfig:
         with ctl.solve(on_model=mcb) as hnd:
             assert hnd.get().satisfiable
         assert mcb.symbols == res
+
+    def test_extend(self):
+        """
+        Test extending config objects.
+        """
+        ctl = Control(self.lib)
+        cfg = ctl.config
+        pc = PluginConfig()
+
+        cfg.add_entry("plugin", "a plugin that needs configuration")
+        cfg.add_entry("plugin.group", "a group", pc.get_group, pc.set_group)
+        cfg.add_entry("plugin.group.val", "a value", pc.get_val, pc.set_val)
+        cfg.add_entry(
+            "plugin.array[]", "an array", set=pc.set_arr_len, size=pc.get_arr_len
+        )
+        cfg.add_entry(
+            "plugin.array.val", "an array value", pc.get_arr_val, pc.set_arr_val
+        )
+
+        assert str(cfg.plugin.group.val.value) == "default value"
+        cfg.plugin.group.val = "new value"
+        assert str(cfg.plugin.group.value) == "some group"
+        assert str(cfg.plugin.group.val) == '"new value"'
+        assert cfg.plugin.group.val.value == "new value"
+        cfg.plugin.group.value = "test"
+        assert cfg.plugin.group.value == "test"
+
+        assert len(cfg.plugin.array) == 1
+        cfg.plugin.array.value = "2"
+        assert len(cfg.plugin.array) == 2
+
+        assert cfg.plugin.array[0].val.value == "default value"
+        assert cfg.plugin.array.val.value == "undefined"
+        cfg.plugin.array[1].val = "5"
+
+        assert str(cfg.plugin) == dedent(
+            """\
+        array:
+          - val: "default value"
+          - val: "5"
+        group: "test"
+               val: "new value"
+        """.rstrip()
+        )
