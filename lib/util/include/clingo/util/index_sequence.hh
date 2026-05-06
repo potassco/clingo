bff457c68f56e23a0e58ef4bdab2a83eced52612
Roland Kaminski <rkaminsk@users.noreply.github.com>
2026-05-06 17:41:26 +0200

    Fix index sequence (#632)
    * fix  tests on latest windows runner
    * fix index sequence
    * add tests for index sequence
    * add tests for programs affected by bug

--- lib/util/include/clingo/util/index_sequence.hh
+++ lib/util/include/clingo/util/index_sequence.hh
@@ -1,8 +1,12 @@
 #pragma once
 
 #include <algorithm>
-#include <tuple>
-#include <vector>
+#include <cassert>
+#include <cstddef>
+#include <iterator>
+#include <utility>
+
+#include <clingo/util/small_vector.hh>
 
 namespace CppClingo::Util {
 
@@ -17,6 +21,7 @@ template <class T> class index_sequence {
     //! Const iterator for index sequence.
     class iterator {
       public:
+        friend class index_sequence;
         //! The iterator category.
         using iterator_category = std::forward_iterator_tag;
         //! The value type.
@@ -32,12 +37,13 @@ template <class T> class index_sequence {
         iterator() = default;
 
         //! Get the current value.
-        auto operator*() const -> reference { return std::get<2>(*it_) + static_cast<T>(idx_); }
+        auto operator*() const -> reference { return it_->second + static_cast<T>(idx_ - start_); }
 
         //! Increment the iterator.
         auto operator++() -> iterator & {
             ++idx_;
-            if (idx_ == std::get<1>(*it_)) {
+            if (idx_ == it_->first) {
+                start_ = it_->first;
                 ++it_;
             }
             return *this;
@@ -57,71 +63,80 @@ template <class T> class index_sequence {
         auto operator!=(const iterator &other) const -> bool { return !(*this == other); }
 
       private:
-        using interval_iterator = std::vector<std::tuple<size_t, size_t, T>>::const_iterator;
+        using interval_iterator = small_vector<std::pair<size_t, T>, 1>::const_iterator;
 
-        iterator(interval_iterator it, size_t idx) : it_{it}, idx_{idx} {}
+        iterator(interval_iterator it, size_t idx, size_t start) : it_{it}, idx_{idx}, start_{start} {}
 
         interval_iterator it_;
         size_t idx_ = 0;
+        size_t start_ = 0;
     };
 
     //! Add an integer to the sequence.
     void add(T value) {
         if (values_.empty()) {
-            values_.emplace_back(0, 1, value);
+            values_.emplace_back(1, value);
         } else {
-            auto &[l, r, y] = values_.back();
-            if (static_cast<T>(r) + y == value) {
+            auto &[r, s] = values_.back();
+            auto l = values_.size() > 1 ? values_[values_.size() - 2].first : size_t{0};
+            if (s + static_cast<T>(r - l) == value) {
                 ++r;
             } else {
-                values_.emplace_back(size(), size() + 1, value - static_cast<T>(size()));
+                values_.emplace_back(size() + 1, value);
             }
         }
     }
 
-    //! Set the sequence to the interval `[l,r-1]`.
-    void assign(T l, T r) {
+    //! Set the sequence to the interval `[s, e)`.
+    void assign(T s, T e) {
+        last_ = 0;
         values_.clear();
-        if (l < r) {
-            values_.emplace_back(0, static_cast<size_t>(r - l), l);
+        if (s < e) {
+            values_.emplace_back(static_cast<size_t>(e - s), s);
         }
     }
 
     //! Get the i-th integer in the sequence.
     [[nodiscard]] auto operator[](size_t index) const -> T {
         assert(index < size() && last_ < values_.size());
-        auto const &[l, r, y] = values_[last_];
+        auto l = last_ > 0 ? values_[last_ - 1].first : size_t{0};
+        auto r = values_[last_].first;
         assert(l < r);
         auto ib = index < l ? values_.begin() : values_.begin() + last_;
         auto ie = index < r ? values_.begin() + last_ + 1 : values_.end();
-        auto it = std::upper_bound(ib, ie, index, [](auto const &a, auto const &b) { return a < std::get<1>(b); });
+        auto it = std::upper_bound(ib, ie, index, [](auto const &a, auto const &b) { return a < b.first; });
         last_ = static_cast<size_t>(std::distance(values_.begin(), it));
-        return static_cast<T>(index) + std::get<2>(*it);
+        auto start = last_ > 0 ? values_[last_ - 1].first : size_t{0};
+        return it->second + static_cast<T>(index - start);
     }
-    //! Check if the sequence contains an element.
-    [[nodiscard]] auto find(T value) const -> size_t {
+    //! Get the index of the value in the sequence.
+    //!
+    //! Returns size() if the value is not found.
+    [[nodiscard]] auto find(T x) const -> size_t {
         // NOTE: has linear complexity in the worst case:
         // - locality could be improved
         // - reverse mapping could be stored
-        for (auto const &[l, r, y] : values_) {
-            auto i = static_cast<size_t>(value - y);
-            if (y <= value && l <= i && i < r) {
-                return i;
+        size_t l = 0;
+        for (auto const &[r, s] : values_) {
+            auto e = s + static_cast<T>(r - l);
+            if (s <= x && x < e) {
+                return l + static_cast<size_t>(x - s);
             }
+            l = r;
         }
         return size();
     }
     //! Get the number of indices in the sequence.
-    [[nodiscard]] auto size() const -> size_t { return empty() ? 0 : std::get<1>(values_.back()); }
+    [[nodiscard]] auto size() const -> size_t { return empty() ? 0 : values_.back().first; }
     //! Check whether the sequence is empty.
     [[nodiscard]] auto empty() const -> bool { return values_.empty(); }
     //! Get an iterator to the beginning of the sequence.
-    [[nodiscard]] auto begin() const -> iterator { return {values_.cbegin(), 0}; }
+    [[nodiscard]] auto begin() const -> iterator { return {values_.cbegin(), 0, 0}; }
     //! Get an iterator to the end of the sequence.
-    [[nodiscard]] auto end() const -> iterator { return {values_.cend(), size()}; }
+    [[nodiscard]] auto end() const -> iterator { return {values_.cend(), size(), size()}; }
 
   private:
-    std::vector<std::tuple<size_t, size_t, T>> values_;
+    small_vector<std::pair<size_t, T>, 1> values_;
     size_t mutable last_ = 0;
 };
 
---
+++ lib/util/tests/index_sequence.cc
@@ -0,0 +1,46 @@
+#include "clingo/util/index_sequence.hh"
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <random>
+
+namespace CppClingo::Util::Test {
+
+TEST_CASE("index sequence", "[base]") {
+    auto x = index_sequence<size_t>{};
+    x.add(1);
+    x.add(0);
+    REQUIRE(x.find(1) == 0);
+    REQUIRE(x.find(0) == 1);
+    REQUIRE(x[0] == 1);
+    REQUIRE(x[1] == 0);
+}
+
+TEST_CASE("index sequence random", "[base]") {
+    static constexpr size_t n = 1000;
+    auto vec = std::vector<size_t>(n);
+    size_t i = 0;
+    for (auto &x : vec) {
+        x = i;
+        ++i;
+    }
+    auto gen = std::mt19937_64{};
+    std::ranges::shuffle(vec, gen);
+    auto seq = index_sequence<size_t>{};
+    for (auto const &x : vec) {
+        seq.add(x);
+    }
+    i = 0;
+    for (auto const &x : vec) {
+        REQUIRE(seq[i] == x);
+        REQUIRE(seq.find(x) == i);
+        ++i;
+    }
+    auto it = vec.begin();
+    for (auto const &x : seq) {
+        REQUIRE(x == *it);
+        ++it;
+    }
+}
+
+} // namespace CppClingo::Util::Test
