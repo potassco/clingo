1fdfc6b8aa81e3ecd86a5ebd8730768a1cd03bc8
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2026-05-07 10:39:20 +0200

    another try

--- .github/conda/bld.bat
+++ .github/conda/bld.bat
@@ -1,4 +1,4 @@
-cmake -S . -B build -G "Ninja Multi-Config" ^
+cmake -S . -B build -G "Visual Studio 18 2026" ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DCMAKE_INSTALL_LIBDIR="lib" ^
     -DPython_ROOT_DIR="%PREFIX%" ^
